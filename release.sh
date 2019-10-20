b3dba01f16b1becceae4c36cd083aaed9a9fbe7e
Vladimir Sapronov <vladimir@specgen.io>
2019-10-19 21:11:18 -0400

    Fixing logging

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ else
     exit 1
 fi
 
-echo 'Releasing $VERSION'
+echo "Releasing $VERSION"
 
 if [ -n "$2" ]; then
     GITHUB_TOKEN=$2
@@ -21,10 +21,14 @@ RELEASE_NAME=$VERSION
 
 go get github.com/aktau/github-release
 
+echo "Creating release in Github: $VERSION"
 $GOPATH/bin/github-release release --security-token $GITHUB_TOKEN --user ModaOperandi --repo specgen --tag $RELEASE_NAME
 
+echo "Releasing zips/specgen_darwin_amd64.zip"
 $GOPATH/bin/github-release upload  --security-token $GITHUB_TOKEN --user ModaOperandi --repo specgen --tag $RELEASE_NAME --name specgen_darwin_amd64.zip  --file zips/specgen_darwin_amd64.zip
+echo "Releasing zips/specgen_linux_amd64.zip"
 $GOPATH/bin/github-release upload  --security-token $GITHUB_TOKEN --user ModaOperandi --repo specgen --tag $RELEASE_NAME --name specgen_linux_amd64.zip   --file zips/specgen_linux_amd64.zip
+echo "Releasing zips/specgen_windows_amd64.zip"
 $GOPATH/bin/github-release upload  --security-token $GITHUB_TOKEN --user ModaOperandi --repo specgen --tag $RELEASE_NAME --name specgen_windows_amd64.zip --file zips/specgen_windows_amd64.zip
 
-echo 'Done releasing $VERSION'
+echo "Done releasing $VERSION"
