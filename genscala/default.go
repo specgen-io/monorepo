ebace4df97084753bb9ae2bb7230c7a5040b2d01
Vladimir Sapronov <vladimir@specgen.io>
2019-10-28 01:04:12 -0400

    Convert to default value in Scala

---
+++ genscala/default.go
@@ -0,0 +1,58 @@
+package genscala
+
+import (
+	"fmt"
+	"github.com/ModaOperandi/spec"
+)
+
+func DefaultValue(typ *spec.Type, value string, modelsMap ModelsMap) string {
+	switch typ.Node {
+	case spec.PlainType:
+		var typeModel *spec.NamedModel = nil
+		if model, ok := modelsMap[typ.PlainType]; ok {
+			typeModel = &model
+		}
+		return PlainScalaValue(typ.PlainType, typeModel, value)
+	default:
+		panic(fmt.Sprintf("Type: %v does not support default value", typ))
+	}
+}
+
+func PlainScalaValue(typ string, model *spec.NamedModel, value string) string {
+	switch typ {
+	case
+		spec.TypeByte,
+		spec.TypeShort,
+		spec.TypeInt16,
+		spec.TypeInt,
+		spec.TypeInt32,
+		spec.TypeLong,
+		spec.TypeInt64,
+		spec.TypeDouble:
+		return value
+	case spec.TypeFloat:
+		return value + "f"
+	case spec.TypeDecimal:
+		return `BigDecimal("` + value + `")`
+	case spec.TypeBool, spec.TypeBoolean:
+		return value
+	case spec.TypeChar:
+		return "'" + value + "'"
+	case spec.TypeString, spec.TypeStr:
+		return `"` + value + `"`
+	case spec.TypeUuid:
+		return `UUID.fromString("` + value + `")`
+	case spec.TypeDate:
+		return `LocalDate.parse("` + value + `", DateTimeFormatter.ISO_LOCAL_DATE)`
+	case spec.TypeDateTime:
+		return `LocalDateTime.parse("` + value + `", DateTimeFormatter.ISO_LOCAL_DATE_TIME)`
+	case spec.TypeTime:
+		return `LocalTime.parse("` + value + `", DateTimeFormatter.ISO_LOCAL_TIME)`
+	default:
+		if model != nil && model.IsEnum() {
+			return model.Name.PascalCase() + `.` + spec.Name{value}.PascalCase()
+		} else {
+			panic(fmt.Sprintf("Type: %v does not support default value", typ))
+		}
+	}
+}
