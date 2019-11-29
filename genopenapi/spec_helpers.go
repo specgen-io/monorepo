6f1bd6ec0f16818921fe6538966608c77973deeb
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2019-11-28 23:57:11 -0500

    Upgraded spec

---
+++ genopenapi/spec_helpers.go
@@ -0,0 +1,36 @@
+package genopenapi
+
+import "github.com/ModaOperandi/spec"
+
+func NewName(source string) spec.Name {
+	return spec.Name{Source: source, Location: nil}
+}
+
+func NewField(name string, typ spec.Type, defaultValue *string, description *string) *spec.NamedField {
+	return &spec.NamedField{
+		Name:              NewName(name),
+		DefinitionDefault: spec.DefinitionDefault{spec.TypeLocated{Definition: typ}, defaultValue, description, nil},
+	}
+}
+
+func NewObject(fields spec.Fields, description *string) *spec.Object {
+	return &spec.Object{fields, description}
+}
+
+func NewParam(name string, typ spec.Type, defaultValue *string, description *string) *spec.NamedParam {
+	return &spec.NamedParam{
+		Name:              NewName(name),
+		DefinitionDefault: spec.DefinitionDefault{spec.TypeLocated{Definition: typ}, defaultValue, description, nil},
+	}
+}
+
+func NewResponse(name string, typ spec.Type, description *string) *spec.NamedResponse {
+	return &spec.NamedResponse{
+		Name:            NewName(name),
+		Definition: spec.Definition{spec.TypeLocated{Definition: typ}, description, nil},
+	}
+}
+
+func NewEnumItem(name string, description *string) *spec.NamedEnumItem {
+	return &spec.NamedEnumItem{NewName(name), spec.EnumItem{Description: description}}
+}
