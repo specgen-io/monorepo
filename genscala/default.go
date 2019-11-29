668bff1021e485744d19f8c1b066ebc67f3c6578
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2019-11-29 16:54:25 -0500

    array and map are defaultable

--- genscala/default.go
+++ genscala/default.go
@@ -8,45 +8,53 @@ import (
 
 func DefaultValue(typ *spec.TypeDef, value string) string {
 	switch typ.Node {
-	case spec.PlainType:
-		return PlainScalaValue(typ.Plain, typ.Info.Model, value)
-	default:
-		panic(fmt.Sprintf("Type: %v does not support default value", typ))
-	}
-}
-
-func PlainScalaValue(typ string, model *spec.NamedModel, value string) string {
-	switch typ {
-	case
-		spec.TypeByte,
-		spec.TypeInt16,
-		spec.TypeInt32,
-		spec.TypeInt64,
-		spec.TypeDouble:
-		return value
-	case spec.TypeFloat:
-		return value + "f"
-	case spec.TypeDecimal:
-		return `BigDecimal("` + value + `")`
-	case spec.TypeBoolean:
-		return value
-	case spec.TypeChar:
-		return "'" + value + "'"
-	case spec.TypeString:
-		return `"` + value + `"`
-	case spec.TypeUuid:
-		return `UUID.fromString("` + value + `")`
-	case spec.TypeDate:
-		return `LocalDate.parse("` + value + `", DateTimeFormatter.ISO_LOCAL_DATE)`
-	case spec.TypeDateTime:
-		return `LocalDateTime.parse("` + value + `", DateTimeFormatter.ISO_LOCAL_DATE_TIME)`
-	case spec.TypeTime:
-		return `LocalTime.parse("` + value + `", DateTimeFormatter.ISO_LOCAL_TIME)`
-	default:
-		if model != nil && model.IsEnum() {
-			return model.Name.PascalCase() + `.` + casee.ToPascalCase(value)
+	case spec.ArrayType:
+		if value == "[]" {
+			return "List()"
 		} else {
-			panic(fmt.Sprintf("Type: %v does not support default value", typ))
+			panic(fmt.Sprintf("Array type %s default value %s is not supported", typ.Name, value))
 		}
+	case spec.MapType:
+		if value == "{}" {
+			return "Map()"
+		} else {
+			panic(fmt.Sprintf("Map type %s default value %s is not supported", typ.Name, value))
+		}
+	case spec.PlainType:
+		switch typ.Plain {
+		case
+			spec.TypeByte,
+			spec.TypeInt16,
+			spec.TypeInt32,
+			spec.TypeInt64,
+			spec.TypeDouble:
+			return value
+		case spec.TypeFloat:
+			return value + "f"
+		case spec.TypeDecimal:
+			return `BigDecimal("` + value + `")`
+		case spec.TypeBoolean:
+			return value
+		case spec.TypeChar:
+			return "'" + value + "'"
+		case spec.TypeString:
+			return `"` + value + `"`
+		case spec.TypeUuid:
+			return `UUID.fromString("` + value + `")`
+		case spec.TypeDate:
+			return `LocalDate.parse("` + value + `", DateTimeFormatter.ISO_LOCAL_DATE)`
+		case spec.TypeDateTime:
+			return `LocalDateTime.parse("` + value + `", DateTimeFormatter.ISO_LOCAL_DATE_TIME)`
+		case spec.TypeTime:
+			return `LocalTime.parse("` + value + `", DateTimeFormatter.ISO_LOCAL_TIME)`
+		default:
+			if typ.Info.Model != nil && typ.Info.Model.IsEnum() {
+				return typ.Info.Model.Name.PascalCase() + `.` + casee.ToPascalCase(value)
+			} else {
+				panic(fmt.Sprintf("Type: %s does not support default value", typ.Name))
+			}
+		}
+	default:
+		panic(fmt.Sprintf("Type: %s does not support default value", typ.Name))
 	}
 }
