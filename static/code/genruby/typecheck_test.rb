e8a14add428b55e99ce6b8633809ab87a8ee4ec0
Vladimir Sapronov <vladimir@specgen.io>
2020-01-28 10:30:31 -0500

    New typecheck library

--- static/code/genruby/typecheck_test.rb
+++ static/code/genruby/typecheck_test.rb
@@ -2,133 +2,123 @@ require "test/unit"
 require "date"
 
 require_relative './typecheck'
+require_relative './enum'
 
-class TypePlainTest < Test::Unit::TestCase
-  def test_plain_success
-    assert_equal "the string", Type.check(String, "the string"), "Plain String type should allow String value"
-  end
-
-  def test_plain_nil
-    assert_raise Type::TypeMismatchException do
-      Type.check(String, nil)
+class TypeCheckTest < Test::Unit::TestCase
+  def test_nil
+    assert_raise TypeError do
+      T.check(String, nil)
     end
   end
 
-  def test_plain_types_mismatch
-    assert_raise Type::TypeMismatchException do
-      Type.check(String, 123)
+  def test_types_mismatch
+    assert_raise TypeError do
+      T.check(String, 123)
     end
   end
 
-  def test_plain_boolean
-    assert_equal true, Type.check(Boolean, true), "Artificial Boolean type should allow true value"
+  def test_string
+    assert_equal "the string", T.check(String, "the string"), "Plain String type should allow String value"
   end
 
-  def test_plain_date
-    assert_equal Date.new(2020, 5, 24), Type.check(Date, Date.new(2020, 5, 24)), "Date type should pass validation"
+  def test_string_nil
+    err = assert_raise TypeError do
+      T.check(String, nil)
+    end
+    assert_match "Type String does not allow nil value", err.message
   end
 
-  def test_plain_datetime
-    assert_equal DateTime.new(2020, 5, 24, 14, 30, 30), Type.check(Date, DateTime.new(2020, 5, 24, 14, 30, 30)), "DateTime type should pass validation"
+  def test_boolean
+    assert_equal true, T.check(Boolean, true), "Artificial Boolean type should allow true value"
   end
 
-  def test_plain_time
-    assert_equal Time.new(2007,11,5,13,45,0, "-05:00"), Type.check(Time, Time.new(2007,11,5,13,45,0, "-05:00")), "Time type should pass validation"
+  def test_date
+    assert_equal Date.new(2020, 5, 24), T.check(Date, Date.new(2020, 5, 24)), "Date type should pass validation"
   end
 
-  def test_plain_uuid
-    assert_equal "123e4567-e89b-12d3-a456-426655440000", Type.check(UUID, "123e4567-e89b-12d3-a456-426655440000"), "UUID type should pass validation on correctly formatted string"
+  def test_datetime
+    assert_equal DateTime.new(2020, 5, 24, 14, 30, 30), T.check(Date, DateTime.new(2020, 5, 24, 14, 30, 30)), "DateTime type should pass validation"
   end
 
-  def test_plain_uuid_fail
-    assert_raise Type::TypeMismatchException do
-      Type.check(UUID, "really not the uuid")
+  def test_time
+    assert_equal Time.new(2007,11,5,13,45,0, "-05:00"), T.check(Time, Time.new(2007, 11, 5, 13, 45, 0, "-05:00")), "Time type should pass validation"
+  end
+
+  def test_uuid
+    assert_equal "123e4567-e89b-12d3-a456-426655440000", T.check(UUID, "123e4567-e89b-12d3-a456-426655440000"), "UUID type should pass validation on correctly formatted string"
+  end
+
+  def test_uuid_fail
+    assert_raise TypeError do
+      T.check(UUID, "really not the uuid")
     end
   end
 
-  def test_plain_enum_success
-    assert_equal Enum::FIRST, Type.check(Enum, Enum::FIRST), "Plain enum type should pass type check"
+  def test_enum_success
+    assert_equal TheEnum::FIRST, T.check(TheEnum, TheEnum::FIRST), "Plain enum type should pass type check"
   end
 
-  def test_plain_enum_fail
-    assert_raise Type::TypeMismatchException do
-      Type.check(Enum, "non existing")
+  def test_enum_fail
+    assert_raise TypeError do
+      T.check(TheEnum, "non existing")
     end
   end
 
-  def test_plain_any_success
-    assert_equal "bla", Type.check(Any, "bla"), "Any type should accept strings"
+  def test_untyped_success
+    assert_equal "bla", T.check(Untyped, "bla"), "Untyped should accept strings"
   end
 
-  def test_plain_any_nil
-    assert_raise Type::TypeMismatchException do
-      Type.check(Any, nil)
+  def test_untyped_nil
+    assert_raise TypeError do
+      T.check(Untyped, nil)
     end
   end
-end
 
-class TypeNillableTest < Test::Unit::TestCase
-  def test_nillable_nil
-    assert_equal nil, Type.check(Type.nillable(String), nil), "Nillable type should allow nil value"
+  def test_nilable_nil
+    assert_equal nil, T.check(T.nilable(String), nil), "Nilable type should allow nil value"
   end
 
-  def test_nillable_straight
-    assert_equal "the string", Type.check(Type.nillable(String), "the string"), "Nillable String type should allow String value"
+  def test_nilable
+    assert_equal "the string", T.check(T.nilable(String), "the string"), "Nilable String type should allow String value"
   end
 
-  def test_nillable_plain
-    assert_equal "the string", Type.check(Type.nillable(Type.plain(String)), "the string"), "Nillable String type should allow String value"
-  end
-end
-
-class TypeArrayTest < Test::Unit::TestCase
-  def test_array_plain
-    assert_equal ["the string"], Type.check(Type.array(String), ["the string"]), "Array of String should allow String value"
+  def test_array_string
+    assert_equal ["the string"], T.check(T.array(String), ["the string"]), "Array of String should allow String value"
   end
 
-  def test_array_noarray
-    assert_raise Type::TypeMismatchException do
-      Type.check(Type.array(String), "the string")
+  def test_array_fail
+    assert_raise TypeError do
+      T.check(T.array(String), "the string")
     end
   end
 
   def test_array_wrong_item_type
-    assert_raise Type::TypeMismatchException do
-      Type.check(Type.array(String), ["the string", 123])
+    assert_raise TypeError do
+      T.check(T.array(String), ["the string", 123])
+    end
+  end
+
+  def test_hash_string_string
+    assert_equal({"key" => "the value"}, T.check(T.hashmap(String, String), {"key" => "the value"}), "Hash of String -> String should allow String -> String value")
+  end
+
+  def test_hash_string_untyped
+    assert_equal({"key" => "the value"}, T.check(T.hashmap(String, Untyped), {"key" => "the value"}), "Hash of String -> Untyped should allow String -> String value")
+  end
+
+  def test_any_string_integer
+    assert_equal(123, T.check(T.any(String, Integer), 123), "Any of String, Integer should allow Integer value")
+  end
+
+  def test_any_string_integer_fail
+    assert_raise TypeError do
+      T.check(T.any(String, Integer), true)
     end
   end
 end
 
-class TypeHashTest < Test::Unit::TestCase
-  def test_hash_success
-    assert_equal({"key" => "the value"}, Type.check(Type.hash(String, String), {"key" => "the value"}), "Hash of String -> String should allow String -> String value")
-  end
-
-  def test_hash_string_any
-    assert_equal({"key" => "the value"}, Type.check(Type.hash(String, Any), {"key" => "the value"}), "Hash of String -> Any should allow String -> String value")
-  end
-end
-
-class TypeStringTest < Test::Unit::TestCase
-  def test_plain_string
-    assert_equal "String", Type.plain(String).to_s
-  end
-
-  def test_nillable_string
-    assert_equal "Nillable[String]", Type.nillable(String).to_s
-  end
-
-  def test_array_string
-    assert_equal "Array[String]", Type.array(String).to_s
-  end
-
-  def test_hash_string
-    assert_equal "Hash[String, String]", Type.hash(String, String).to_s
-  end
-end
-
-class Enum
-  include Ruby::Enum
+class TheEnum
+  include Enum
 
   define :FIRST, 'first'
   define :SECOND, 'second'
