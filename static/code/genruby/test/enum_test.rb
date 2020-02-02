ff23a456219502eb94b6426fade7e422f3bff5a7
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2020-02-01 23:24:12 -0500

    Fix for enum test to not clash with models test

--- static/code/genruby/test/enum_test.rb
+++ static/code/genruby/test/enum_test.rb
@@ -7,38 +7,38 @@ require '[[.GemName]]/jsoner'
 module [[.ModuleName]]
   class TypeCheckEnum < Test::Unit::TestCase
     def test_success
-      assert_equal TheEnum::one, T.check(TheEnum, TheEnum::one), "Plain enum type should pass type check"
+      assert_equal SomeEnum::one, T.check(SomeEnum, SomeEnum::one), "Plain enum type should pass type check"
     end
 
     def test_fail
       assert_raise TypeError do
-        T.check(TheEnum, "non existing")
+        T.check(SomeEnum, "non existing")
       end
     end
   end
 
   class EnumDeserialization < Test::Unit::TestCase
     def test_deserialize_enum
-      data = Jsoner.from_json(TheEnum, '"two"')
-      T.check(TheEnum, data)
-      assert_equal TheEnum::two, data, "Enum should be parsable from JSON"
+      data = Jsoner.from_json(SomeEnum, '"two"')
+      T.check(SomeEnum, data)
+      assert_equal SomeEnum::two, data, "Enum should be parsable from JSON"
     end
 
     def test_deserialize_enum_non_existing_item
       assert_raise JsonerError do
-        Jsoner.from_json(TheEnum, '"non_existing"')
+        Jsoner.from_json(SomeEnum, '"non_existing"')
       end
     end
   end
 
   class EnumSerialization < Test::Unit::TestCase
     def test_serialize_enum
-      json_str = Jsoner.to_json(TheEnum, TheEnum::two)
+      json_str = Jsoner.to_json(SomeEnum, SomeEnum::two)
       assert_equal '"two"', json_str, "Enum should be serializable to JSON"
     end
   end
 
-  class TheEnum
+  class SomeEnum
     include Enum
 
     define :one, 'one'
