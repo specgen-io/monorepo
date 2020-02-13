0f7c2b36829fd704c5e3b5e6707fb34b5d889e91
Vladimir Sapronov <vladimir@specgen.io>
2020-02-13 10:29:22 -0500

    ruby: Support for defaulted params

---
+++ genruby/default.go
@@ -0,0 +1,59 @@
+package genruby
+
+import (
+	"fmt"
+	"github.com/ModaOperandi/spec"
+	"github.com/vsapronov/casee"
+)
+
+func DefaultValue(typ *spec.TypeDef, value string) string {
+	switch typ.Node {
+	case spec.ArrayType:
+		if value == "[]" {
+			return "[]"
+		} else {
+			panic(fmt.Sprintf("Array type %s default value %s is not supported", typ.Name, value))
+		}
+	case spec.MapType:
+		if value == "{}" {
+			return "{}"
+		} else {
+			panic(fmt.Sprintf("Map type %s default value %s is not supported", typ.Name, value))
+		}
+	case spec.PlainType:
+		switch typ.Plain {
+		case
+			spec.TypeByte,
+			spec.TypeInt16,
+			spec.TypeInt32,
+			spec.TypeInt64:
+			return value
+		case
+			spec.TypeDouble,
+			spec.TypeFloat,
+			spec.TypeDecimal:
+			return value + ".to_f"
+		case spec.TypeBoolean:
+			return value
+		case
+			spec.TypeChar,
+			spec.TypeString,
+			spec.TypeUuid:
+			return `'` + value + `'`
+		case spec.TypeDate:
+			return `Date.strptime('`+value+`', '%Y-%m-%d')`
+		case spec.TypeDateTime:
+			return `DateTime.strptime('`+value+`', '%Y-%m-%dT%H:%M:%S')`
+		case spec.TypeTime:
+			return `TimeOfDay.parse('`+value+`')`
+		default:
+			if typ.Info.Model != nil && typ.Info.Model.IsEnum() {
+				return typ.Info.Model.Name.PascalCase() + `::` + casee.ToSnakeCase(value)
+			} else {
+				panic(fmt.Sprintf("Type: %s does not support default value", typ.Name))
+			}
+		}
+	default:
+		panic(fmt.Sprintf("Type: %s does not support default value", typ.Name))
+	}
+}
