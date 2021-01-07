ea649780c526cfc6dde2387b3adfddd534a80a8a
Vladimir Sapronov <vladimir@specgen.io>
2021-01-06 22:51:47 -0500

    genscala: sttp client removed dependency from akka libraries

--- plugins/sbt-spec/src/main/scala/Plugins.scala
+++ plugins/sbt-spec/src/main/scala/Plugins.scala
@@ -41,12 +41,8 @@ object SpecKeys {
     "com.beachape" %% "enumeratum-circe" % "1.5.22",
 
     "org.slf4j" % "slf4j-api" % "1.7.28",
-    "org.scala-lang" % "scala-reflect" % "2.12.9",
     "com.softwaremill.sttp" %% "core" % "1.7.1",
-    "com.softwaremill.sttp" %% "akka-http-backend" % "1.7.1",
-    "com.typesafe.akka" %% "akka-stream" % "2.5.23",
   )
-
 }
 
 object CommonKeys extends AutoPlugin {
