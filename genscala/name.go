542bea43f4f43eeb98f83732c18fcb2710389fdd
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-03-29 04:38:47 -0400

    genscala: Fix around keywords in models fields

---
+++ genscala/name.go
@@ -0,0 +1,61 @@
+package genscala
+
+import (
+	"fmt"
+	"github.com/specgen-io/spec"
+)
+
+func scalaCamelCase(name spec.Name) string {
+	return scalaName(name.CamelCase())
+}
+
+func scalaName(name string) string {
+	for _, keyword := range keywords {
+		if name == keyword {
+			return fmt.Sprintf("`%s`", name)
+		}
+	}
+	return name
+}
+
+var keywords = []string{
+	"abstract",
+	"case",
+	"catch",
+	"class",
+	"def",
+	"do",
+	"else",
+	"extends",
+	"false",
+	"final",
+	"finally",
+	"for",
+	"forSome",
+	"if",
+	"implicit",
+	"import",
+	"lazy",
+	"match",
+	"new",
+	"null",
+	"object",
+	"override",
+	"package",
+	"private",
+	"protected",
+	"return",
+	"sealed",
+	"super",
+	"this",
+	"throw",
+	"trait",
+	"try",
+	"true",
+	"type",
+	"val",
+	"var",
+	"while",
+	"with",
+	"yield",
+}
