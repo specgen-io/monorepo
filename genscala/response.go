e01b4be8a2808308dfefc7d4e964ef62039a3421
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-04-26 17:30:27 -0400

    Removed OperationResult from sttp client

--- genscala/response.go
+++ genscala/response.go
@@ -1,25 +1,12 @@
 package genscala
 
 import (
-	"fmt"
 	spec "github.com/specgen-io/spec.v2"
 	"github.com/vsapronov/gopoetry/scala"
 )
 
 func responseType(operation spec.NamedOperation) string {
 	return operation.Name.PascalCase() + "Response"
-}
-
-func generateResponseCases(responses spec.Responses) *scala.StatementsDeclaration {
-	cases := scala.Statements()
-	for _, response := range responses {
-		responseParam := ``
-		if !response.Type.Definition.IsEmpty() {
-			responseParam = fmt.Sprintf(`Jsoner.read[%s](result.body.get)`, ScalaType(&response.Type.Definition))
-		}
-		cases.Add(Line(`case %s => %s(%s)`, spec.HttpStatusCode(response.Name), response.Name.PascalCase(), responseParam))
-	}
-	return cases
 }
 
 func generateResponse(responseTypeName string, responses spec.Responses) (*scala.TraitDeclaration, *scala.ClassDeclaration) {
