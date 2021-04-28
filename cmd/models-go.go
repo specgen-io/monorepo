b3c8cf4eeb010c78724cd3ebd0ac900b41cc143b
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-04-28 13:01:11 -0400

    gengo: Simplest models gen code skeleton

---
+++ cmd/models-go.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"specgen/fail"
+	"specgen/gengo"
+)
+
+func init() {
+	modelsGo.Flags().String(SpecFile, "", SpecFileDescription)
+	modelsGo.Flags().String(GeneratePath, "", GeneratePathDescription)
+
+	modelsGo.MarkFlagRequired(SpecFile)
+	modelsGo.MarkFlagRequired(GeneratePath)
+
+	rootCmd.AddCommand(modelsGo)
+}
+
+var modelsGo = &cobra.Command{
+	Use:   "models-go",
+	Short: "Generate Go models source code",
+	Run: func(cmd *cobra.Command, args []string) {
+		specFile, err := cmd.Flags().GetString(SpecFile)
+		fail.IfError(err)
+
+		generatePath, err := cmd.Flags().GetString(GeneratePath)
+		fail.IfError(err)
+
+		err = gengo.GenerateModels(specFile, generatePath)
+		fail.IfErrorF(err, "Failed to generate models code")
+	},
+}
---
+++ gengo/models.go
@@ -0,0 +1,6 @@
+package gengo
+
+func GenerateModels(serviceFile string, generatePath string) error {
+	return nil
+}
+
