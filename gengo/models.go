d68889f229175428897c0358db49d5d7a7e700de
Anastasiya <nislovskaya96@gmail.com>
2021-05-13 01:42:12 +0300

    gengo: Fixed generation of models, added generation of functions

--- gengo/models.go
+++ gengo/models.go
@@ -38,6 +38,8 @@ func generateModels(specification *spec.Spec, packageName string, generatePath s
 		w.Line("import (")
 		w.Line(`  "cloud.google.com/go/civil"`)
 		w.Line(`  "encoding/json"`)
+		w.Line(`  "errors"`)
+		w.Line(`  "fmt"`)
 		w.Line(`  "github.com/google/uuid"`)
 		w.Line(`  "github.com/shopspring/decimal"`)
 		w.Line(")")
@@ -51,6 +53,7 @@ func generateModels(specification *spec.Spec, packageName string, generatePath s
 				generateEnumModel(w, model)
 			}
 		}
+		generateHelperFunctions(w)
 		file := gen.TextFile{
 			Path:    generatePath,
 			Content: w.String(),
@@ -74,26 +77,56 @@ func generateEnumModel(w *gen.Writer, model *spec.NamedModel) {
 	w.Line("type %s %s", model.Name.PascalCase(), "string")
 	w.Line("")
 	w.Line("const (")
-	var modelName string
+	modelName := model.Name.PascalCase()
 	choiceValuesStringsParams := []string{}
 	choiceValuesParams := []string{}
 	for _, enumItem := range model.Enum.Items {
-		modelName = model.Name.PascalCase()
-		w.Line("  %s %s = \"%s\"", enumItem.Name.PascalCase(), modelName, enumItem.Value)
-		choiceValuesStringsParams = append(choiceValuesStringsParams, fmt.Sprintf("string(%s)", enumItem.Name.PascalCase()))
-		choiceValuesParams = append(choiceValuesParams, fmt.Sprintf("%s", enumItem.Name.PascalCase()))
+		enumItemName := enumItem.Name.PascalCase()
+		w.Line("  %s %s = \"%s\"", enumItemName, modelName, enumItem.Value)
+		choiceValuesStringsParams = append(choiceValuesStringsParams, fmt.Sprintf("string(%s)", enumItemName))
+		choiceValuesParams = append(choiceValuesParams, fmt.Sprintf("%s", enumItemName))
 	}
 	w.Line(")")
 	w.Line("")
 	w.Line("var %sValuesStrings = []string{%s}", modelName, strings.Join(choiceValuesStringsParams, ", "))
 	w.Line("var %sValues = []%s{%s}", modelName, modelName, strings.Join(choiceValuesParams, ", "))
+	w.Line("")
+	w.Line("func (self *%s) UnmarshalJSON(b []byte) error {", modelName)
+	w.Line("  str, err := readEnumStringValue(b, %sValuesStrings)", modelName)
+	w.Line("  if err != nil { return err }")
+	w.Line("  *self = %s(str)", modelName)
+	w.Line("  return nil")
+	w.Line("}")
+}
+
+func generateHelperFunctions(w *gen.Writer) {
+	w.Line("")
+	w.Line("func contains(lookFor string, arr []string) bool {")
+	w.Line("  for _, value := range arr{")
+	w.Line("    if lookFor == value {")
+	w.Line("      return true")
+	w.Line("    }")
+	w.Line("  }")
+	w.Line("  return false")
+	w.Line("}")
+	w.Line("")
+	w.Line("func readEnumStringValue(b []byte, values []string) (string, error) {")
+	w.Line("  var str string")
+	w.Line("  if err := json.Unmarshal(b, &str); err != nil {")
+	w.Line("    return \"\", err")
+	w.Line("  }")
+	w.Line("  if !contains(str, values) {")
+	w.Line("    return \"\", errors.New(fmt.Sprintf(\"Unknown enum value: %ss\", str))", "%")
+	w.Line("  }")
+	w.Line("  return str, nil")
+	w.Line("}")
 }
 
 func generateOneOfModel(w *gen.Writer, model *spec.NamedModel) {
 	w.Line("type %s struct {", model.Name.PascalCase())
 	for _, item := range model.OneOf.Items {
 		typ := GoType(&item.Type.Definition)
-		w.Line("  %s %s `json:\"%s\"`", item.Name.PascalCase(), typ, item.Name.Source)
+		w.Line("  %s *%s `json:\"%s,omitempty\"`", item.Name.PascalCase(), typ, item.Name.Source)
 	}
 	w.Line("}")
 }
