ae9ab80bbbdb03103dadc3d51281622b39905099
Vladimir Sapronov <vladimir@specgen.io>
2021-05-14 12:19:19 -0400

    gents: Added models-ts-iots command

---
+++ cmd/models-ts-iots.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"specgen/fail"
+	"specgen/gents"
+)
+
+func init() {
+	cmdModelsTsIoTs.Flags().String(SpecFile, "", SpecFileDescription)
+	cmdModelsTsIoTs.Flags().String(GeneratePath, "", GeneratePathDescription)
+
+	cmdModelsTsIoTs.MarkFlagRequired(SpecFile)
+	cmdModelsTsIoTs.MarkFlagRequired(GeneratePath)
+
+	rootCmd.AddCommand(cmdModelsTsIoTs)
+}
+
+var cmdModelsTsIoTs = &cobra.Command{
+	Use:   "models-ts-iots",
+	Short: "Generate TypeScript io-ts models",
+	Run: func(cmd *cobra.Command, args []string) {
+		specFile, err := cmd.Flags().GetString(SpecFile)
+		fail.IfError(err)
+
+		generatePath, err := cmd.Flags().GetString(GeneratePath)
+		fail.IfError(err)
+
+		err = gents.GenerateIoTsModels(specFile, generatePath)
+		fail.IfErrorF(err, "Failed to generate code")
+	},
+}
