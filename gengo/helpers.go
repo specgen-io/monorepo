b1f620a2172b2b09c8ba91cf28ddaccf52cc924b
Anastasiya Nislovskaya <anastasiya@specgen.io>
2021-05-19 19:02:08 +0300

    gengo: Сompleted generation of models (#13)
    * gengo: Simplest models gen code skeleton
    
    * gengo: Added types, default types and type test
    
    * gengo: ÐFixed type_test and added GenerateModels
    
    * gengo: Added models generation
    
    * gengo: Added writer
    
    * gengo: Fixed generateModels
    
    * gengo: Fixed models, type ann type test
    
    * gengo: Fixed models
    
    * Fixed for the latest spec v2 format
    
    * gengo: Fixed generation of models
    
    * gengo: Fixed types and generation of models
    
    * gengo: Fixed generation of models, added generation of functions
    
    * gengo: Added generation of helpers and fixed generation of models
    
    * gengo: Fixed generation of models
    
    * gengo: Fixed generation of imports
    
    * gengo: Fixed generation of imports
    
    * gengo: Fixed generation of import
    
    * gengo: Fixed generation of imports
    
    * gengo: Minor fixes for models generation
    
    Co-authored-by: Vladimir Sapronov <vladimir.sapronov@gmail.com>

---
+++ gengo/helpers.go
@@ -0,0 +1,40 @@
+package gengo
+
+import (
+	"specgen/gen"
+	"strings"
+)
+
+func generateHelperFunctions(packageName string, path string) *gen.TextFile {
+	code := `
+package [[.PackageName]]
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+func contains(lookFor string, arr []string) bool {
+	for _, value := range arr{
+		if lookFor == value {
+			return true
+		}
+	}
+	return false
+}
+
+func readEnumStringValue(b []byte, values []string) (string, error) {
+	var str string
+	if err := json.Unmarshal(b, &str); err != nil {
+		return "", err
+	}
+	if !contains(str, values) {
+		return "", errors.New(fmt.Sprintf("Unknown enum value: %s", str))
+	}
+	return str, nil
+}
+`
+	code, _ = gen.ExecuteTemplate(code, struct { PackageName string } {packageName })
+	return &gen.TextFile{path, strings.TrimSpace(code)}
+}
