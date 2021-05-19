7543690c24678449306d13b2aa43e05246f2e6ba
Anastasiya Nislovskaya <70210318+Nislovskaya96@users.noreply.github.com>
2021-05-19 19:02:08 +0300

    gengo: Сompleted generation of models (#13)
    * gengo: Simplest models gen code skeleton
    
    * gengo: Added types, default types and type test
    
    * gengo: ÐFixed type_test and added GenerateModels
    
    * gengo: Added models generation
    
    * gengo: Added writer
    
    * gengo: Fixed generateModels
    
    * gengo: Fixed models, type ann type test
    
    * gengo: Fixed models
    
    * Fixed for the latest spec v2 format
    
    * gengo: Fixed generation of models
    
    * gengo: Fixed types and generation of models
    
    * gengo: Fixed generation of models, added generation of functions
    
    * gengo: Added generation of helpers and fixed generation of models
    
    * gengo: Fixed generation of models
    
    * gengo: Fixed generation of imports
    
    * gengo: Fixed generation of imports
    
    * gengo: Fixed generation of import
    
    * gengo: Fixed generation of imports
    
    * gengo: Minor fixes for models generation
    
    Co-authored-by: Vladimir Sapronov <vladimir.sapronov@gmail.com>

---
+++ gengo/models.go
@@ -0,0 +1,132 @@
+package gengo
+
+import (
+	"fmt"
+	"github.com/specgen-io/spec.v2"
+	"path/filepath"
+	"specgen/gen"
+	"strings"
+)
+
+func GenerateModels(serviceFile string, generatePath string) error {
+	specification, err := spec.ReadSpec(serviceFile)
+	if err != nil {
+		return err
+	}
+	files := []gen.TextFile{}
+	for _, version := range specification.Versions {
+		w := NewGoWriter()
+		generateModels(w, &version, "spec")
+		folder := "spec"
+		if version.Version.Source != "" {
+			folder += "_" + version.Version.FlatCase()
+		}
+		files = append(files, gen.TextFile{Path: filepath.Join(generatePath, folder, "models.go"), Content: w.String()})
+		files = append(files, *generateHelperFunctions("spec", filepath.Join(generatePath, "spec", "helpers.go")))
+	}
+	return gen.WriteFiles(files, true)
+}
+
+func versionedPackage(version spec.Name, packageName string) string {
+	if version.Source != "" {
+		return fmt.Sprintf("%s_%s", packageName, version.FlatCase())
+	}
+	return packageName
+}
+
+func generateImport(w *gen.Writer, version *spec.Version, typ string, importStr string) *gen.Writer {
+	for _, model := range version.ResolvedModels {
+		if model.IsObject() {
+			for _, field := range model.Object.Fields {
+ 				if checkType(&field.Type.Definition, typ) {
+					w.Line(`import "%s"`, importStr)
+					return w
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func checkType(fieldType *spec.TypeDef, typ string) bool {
+	if fieldType.Plain != typ {
+		switch fieldType.Node {
+		case spec.PlainType:
+			return false
+		case spec.NullableType:
+			return checkType(fieldType.Child, typ)
+		case spec.ArrayType:
+			return checkType(fieldType.Child, typ)
+		case spec.MapType:
+			return checkType(fieldType.Child, typ)
+		default:
+			panic(fmt.Sprintf("Unknown type: %v", typ))
+		}
+	}
+	return true
+}
+
+func generateModels(w *gen.Writer, version *spec.Version, packageName string) {
+	w.Line("package %s", versionedPackage(version.Version, packageName))
+	w.EmptyLine()
+	generateImport(w, version, spec.TypeDate, "cloud.google.com/go/civil")
+	generateImport(w, version, spec.TypeJson, "encoding/json")
+	generateImport(w, version, spec.TypeUuid, "github.com/google/uuid")
+	generateImport(w, version, spec.TypeDecimal, "github.com/shopspring/decimal")
+	for _, model := range version.ResolvedModels {
+		if strings.Contains(w.String(), "import") {
+			w.EmptyLine()
+		}
+		if model.IsObject() {
+			generateObjectModel(w, model)
+		} else if model.IsOneOf() {
+			generateOneOfModel(w, model)
+		} else if model.IsEnum() {
+			generateEnumModel(w, model)
+		}
+	}
+}
+
+func generateObjectModel(w *gen.Writer, model *spec.NamedModel) {
+	w.Line("type %s struct {", model.Name.PascalCase())
+	for _, field := range model.Object.Fields {
+		typ := GoType(&field.Type.Definition)
+		w.Line("  %s %s `json:\"%s\"`", field.Name.PascalCase(), typ, field.Name.Source)
+	}
+	w.Line("}")
+}
+
+func generateEnumModel(w *gen.Writer, model *spec.NamedModel) {
+	w.Line("type %s %s", model.Name.PascalCase(), "string")
+	w.EmptyLine()
+	w.Line("const (")
+	modelName := model.Name.PascalCase()
+	choiceValuesStringsParams := []string{}
+	choiceValuesParams := []string{}
+	for _, enumItem := range model.Enum.Items {
+		enumItemName := enumItem.Name.PascalCase()
+		w.Line("  %s %s = \"%s\"", enumItemName, modelName, enumItem.Value)
+		choiceValuesStringsParams = append(choiceValuesStringsParams, fmt.Sprintf("string(%s)", enumItemName))
+		choiceValuesParams = append(choiceValuesParams, fmt.Sprintf("%s", enumItemName))
+	}
+	w.Line(")")
+	w.EmptyLine()
+	w.Line("var %sValuesStrings = []string{%s}", modelName, strings.Join(choiceValuesStringsParams, ", "))
+	w.Line("var %sValues = []%s{%s}", modelName, modelName, strings.Join(choiceValuesParams, ", "))
+	w.EmptyLine()
+	w.Line("func (self *%s) UnmarshalJSON(b []byte) error {", modelName)
+	w.Line("  str, err := readEnumStringValue(b, %sValuesStrings)", modelName)
+	w.Line("  if err != nil { return err }")
+	w.Line("  *self = %s(str)", modelName)
+	w.Line("  return nil")
+	w.Line("}")
+}
+
+func generateOneOfModel(w *gen.Writer, model *spec.NamedModel) {
+	w.Line("type %s struct {", model.Name.PascalCase())
+	for _, item := range model.OneOf.Items {
+		typ := GoType(&item.Type.Definition)
+		w.Line("  %s *%s `json:\"%s,omitempty\"`", item.Name.PascalCase(), typ, item.Name.Source)
+	}
+	w.Line("}")
+}
---
+++ gengo/type.go
@@ -0,0 +1,56 @@
+package gengo
+
+import (
+	"fmt"
+	spec "github.com/specgen-io/spec.v2"
+)
+
+func GoType(typ *spec.TypeDef) string {
+	switch typ.Node {
+	case spec.PlainType:
+		return PlainGoType(typ.Plain)
+	case spec.NullableType:
+		child := GoType(typ.Child)
+		result := "*" + child
+		return result
+	case spec.ArrayType:
+		child := GoType(typ.Child)
+		result := "[]" + child
+		return result
+	case spec.MapType:
+		child := GoType(typ.Child)
+		result := "map[string]" + child
+		return result
+	default:
+		panic(fmt.Sprintf("Unknown type: %v", typ))
+	}
+}
+
+func PlainGoType(typ string) string {
+	switch typ {
+	case spec.TypeInt32:
+		return "int32"
+	case spec.TypeInt64:
+		return "int64"
+	case spec.TypeFloat:
+		return "float32"
+	case spec.TypeDouble:
+		return "float64"
+	case spec.TypeDecimal:
+		return "decimal.Decimal"
+	case spec.TypeBoolean:
+		return "bool"
+	case spec.TypeString:
+		return "string"
+	case spec.TypeUuid:
+		return "uuid.UUID"
+	case spec.TypeDate:
+		return "civil.Date"
+	case spec.TypeDateTime:
+		return "civil.DateTime"
+	case spec.TypeJson:
+		return "json.RawMessage"
+	default:
+		return typ
+	}
+}
