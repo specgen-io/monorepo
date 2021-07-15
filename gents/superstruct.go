b9e84f4a0b25b49b7cbd77dbbca0d51a48f84d3d
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-07-15 17:06:10 -0400

    gents: Renamed Superstruct DateTime to StrDateTime

--- gents/superstruct.go
+++ gents/superstruct.go
@@ -15,8 +15,6 @@ export * from "superstruct"
 
 import * as t from "superstruct"
 
-export const DateTime = t.coerce<Date, unknown, string>(t.date(), t.string(), (value: string): Date => new Date(value))
-
 export const decode = <T>(struct: t.Struct<T, unknown>, value: unknown): T => {
     return t.create(value, struct)
 };
@@ -25,6 +23,7 @@ export const encode = <T>(struct: t.Struct<T, unknown>, value: T): unknown => {
     return JSON.parse(JSON.stringify(value))
 };
 
+export const StrDateTime = t.coerce<Date, unknown, string>(t.date(), t.string(), (value: string): Date => new Date(value))
 export const StrInteger = t.coerce<number, unknown, string>(t.integer(), t.string(), (value: string): number => parseInt(value))
 export const StrFloat = t.coerce<number, unknown, string>(t.number(), t.string(), (value: string): number => parseFloat(value))
 export const StrBoolean = t.coerce<boolean, unknown, string>(t.boolean(), t.string(), (value: string): boolean => {
