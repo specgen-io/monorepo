20ecf3609f26b58cd842c66584122006f1c48e0e
Vladimir Sapronov <vladimir@specgen.io>
2021-07-18 22:48:24 -0400

    gents: Removed semicolon

--- gents/superstruct.go
+++ gents/superstruct.go
@@ -17,11 +17,11 @@ import * as t from "superstruct"
 
 export const decode = <T>(struct: t.Struct<T, unknown>, value: unknown): T => {
     return t.create(value, struct)
-};
+}
 
 export const encode = <T>(struct: t.Struct<T, unknown>, value: T): unknown => {
     return JSON.parse(JSON.stringify(value))
-};
+}
 
 export const StrDateTime = t.coerce<Date, unknown, string>(t.date(), t.string(), (value: string): Date => new Date(value))
 export const StrInteger = t.coerce<number, unknown, string>(t.integer(), t.string(), (value: string): number => parseInt(value))
