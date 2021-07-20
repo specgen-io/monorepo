9abdd482bf03c2f254c1b0fbfdf1cb8dcafa6880
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-07-20 15:17:43 -0400

    genruby: Removed gopoetry/ruby (#26)
    * genruby: Removed gopoetry/ruby
    
    * genruby: Fix typo

--- genruby/models.go
+++ genruby/models.go
@@ -2,9 +2,8 @@ package genruby
 
 import (
 	"fmt"
-	spec "github.com/specgen-io/spec"
+	"github.com/specgen-io/spec"
 	"github.com/specgen-io/specgen/v2/gen"
-	"github.com/vsapronov/gopoetry/ruby"
 	"path/filepath"
 	"strings"
 )
@@ -25,72 +24,66 @@ func GenerateModels(serviceFile string, generatePath string) error {
 }
 
 func generateModels(specification *spec.Spec, moduleName string, generatePath string) *gen.TextFile {
-	unit := ruby.Unit()
-	unit.Require("date")
-	unit.Require("emery")
+	w := NewRubyWriter()
+	w.Line(`require "date"`)
+	w.Line(`require "emery"`)
 
 	for _, version := range specification.Versions {
 		if version.Version.Source == "" {
-			module := generateVersionModelsModule(&version, moduleName)
-			unit.AddDeclarations(module)
+			generateVersionModelsModule(w, &version, moduleName)
 		}
 	}
 
 	for _, version := range specification.Versions {
 		if version.Version.Source != "" {
-			module := generateVersionModelsModule(&version, moduleName)
-			unit.AddDeclarations(module)
+			generateVersionModelsModule(w, &version, moduleName)
 		}
 	}
 
-	return &gen.TextFile{
-		Path:    generatePath,
-		Content: unit.Code(),
-	}
+	return &gen.TextFile{Path: generatePath, Content: w.String()}
 }
 
-func generateVersionModelsModule(version *spec.Version, moduleName string) *ruby.ModuleDeclaration {
-	module := ruby.Module(versionedModule(moduleName, version.Version))
-	for _, model := range version.ResolvedModels {
+func generateVersionModelsModule(w *gen.Writer, version *spec.Version, moduleName string) {
+	w.EmptyLine()
+	w.Line("module %s", versionedModule(moduleName, version.Version))
+	for index, model := range version.ResolvedModels {
+		if index != 0 {
+			w.EmptyLine()
+		}
 		if model.IsObject() {
-			module.AddDeclarations(generateObjectModel(model))
+			generateObjectModel(w.Indented(), model)
 		} else if model.IsOneOf() {
-			module.AddDeclarations(generateOneOfModel(model))
+			generateOneOfModel(w.Indented(), model)
 		} else if model.IsEnum() {
-			module.AddDeclarations(generateEnumModel(model))
+			generateEnumModel(w.Indented(), model)
 		}
 	}
-	return module
+	w.Line("end")
 }
 
-func generateObjectModel(model *spec.NamedModel) ruby.Writable {
-	class := ruby.Class(model.Name.PascalCase())
-	class.AddCode("include DataClass")
-
+func generateObjectModel(w *gen.Writer, model *spec.NamedModel) {
+	w.Line("class %s", model.Name.PascalCase())
+	w.Line("  include DataClass")
 	for _, field := range model.Object.Fields {
 		typ := RubyType(&field.Type.Definition)
-		class.AddCode(fmt.Sprintf("val :%s, %s", field.Name.SnakeCase(), typ))
+		w.Line("  val :%s, %s", field.Name.SnakeCase(), typ)
 	}
-
-	return class
+	w.Line("end")
 }
 
-func generateEnumModel(model *spec.NamedModel) ruby.Writable {
-	class := ruby.Class(model.Name.PascalCase())
-
-	class.AddCode("include Enum")
+func generateEnumModel(w *gen.Writer, model *spec.NamedModel) {
+	w.Line("class %s", model.Name.PascalCase())
+	w.Line("  include Enum")
 	for _, enumItem := range model.Enum.Items {
-		class.AddCode(fmt.Sprintf("define :%s, '%s'", enumItem.Name.SnakeCase(), enumItem.Value))
+		w.Line("  define :%s, '%s'", enumItem.Name.SnakeCase(), enumItem.Value)
 	}
-
-	return class
+	w.Line("end")
 }
 
-func generateOneOfModel(model *spec.NamedModel) ruby.Writable {
+func generateOneOfModel(w *gen.Writer, model *spec.NamedModel) {
 	params := []string{}
 	for _, item := range model.OneOf.Items {
 		params = append(params, fmt.Sprintf("%s: %s", item.Name.SnakeCase(), RubyType(&item.Type.Definition)))
 	}
-	typedefinition := fmt.Sprintf("%s = T.union(%s)", model.Name.PascalCase(), strings.Join(params, ", "))
-	return ruby.Statements().AddLn(typedefinition)
+	w.Line("%s = T.union(%s)", model.Name.PascalCase(), strings.Join(params, ", "))
 }
