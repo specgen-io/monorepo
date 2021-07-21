60ee5f38827bbb1b04f325bdf2a0e279bf14aa18
Anastasiya Nislovskaya <anastasiya@specgen.io>
2021-07-21 18:16:57 +0300

    genjava: Fixed imports and check types

--- genjava/check_types.go
+++ genjava/check_types.go
@@ -6,48 +6,19 @@ import (
 )
 
 func checkType(fieldType *spec.TypeDef, typ string) bool {
-	if fieldType.Plain != typ {
-		switch fieldType.Node {
-		case spec.PlainType:
+	switch fieldType.Node {
+	case spec.PlainType:
+		if fieldType.Plain != typ {
 			return false
-		case spec.NullableType:
-			return checkType(fieldType.Child, typ)
-		case spec.ArrayType:
-			return checkType(fieldType.Child, typ)
-		case spec.MapType:
-			return checkType(fieldType.Child, typ)
-		default:
-			panic(fmt.Sprintf("Unknown type: %v", typ))
 		}
+	case spec.NullableType:
+		return checkType(fieldType.Child, typ)
+	case spec.ArrayType:
+		return checkType(fieldType.Child, typ)
+	case spec.MapType:
+		return checkType(fieldType.Child, typ)
+	default:
+		panic(fmt.Sprintf("Unknown type: %v", typ))
 	}
 	return true
-}
-
-func modelsHasType(model *spec.NamedModel, typ string) bool {
-	if model.IsObject() {
-		for _, field := range model.Object.Fields {
-			if checkType(&field.Type.Definition, typ) {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func checkMapType(fieldType *spec.TypeDef) bool {
-	if fieldType.Node == spec.MapType {
-		return true
-	}
-	return false
-}
-
-func modelsHasMapType(model *spec.NamedModel) bool {
-	if model.IsObject() {
-		for _, field := range model.Object.Fields {
-			if checkMapType(&field.Type.Definition) {
-				return true
-			}
-		}
-	}
-	return false
 }
