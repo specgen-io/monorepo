f44e22da9a702fb92c1ce589376777f34d74d708
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-07-31 22:07:06 -0400

    genscala: Fix for url params parsing

--- genscala/play.go
+++ genscala/play.go
@@ -558,24 +558,41 @@ func generatePlayParams(packageName string, path string) *gen.TextFile {
 	code := `
 package [[.PackageName]]
 
-import play.api.mvc.QueryStringBindable
+import play.api.mvc.{PathBindable, QueryStringBindable}
 
 object PlayParamsTypesBindings {
-  implicit def bindableParser[T](implicit stringBinder: QueryStringBindable[String], codec: Codec[T]): QueryStringBindable[T] = new QueryStringBindable[T] {
+  implicit def queryBindableParser[T](implicit stringBinder: QueryStringBindable[String], codec: Codec[T]): QueryStringBindable[T] = new QueryStringBindable[T] {
     override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, T]] =
       for {
-        dateStr <- stringBinder.bind(key, params)
+        valueStr <- stringBinder.bind(key, params)
       } yield {
-        dateStr match {
-          case Right(value) =>
+        valueStr match {
+          case Right(valueStr) =>
             try {
-              Right(codec.decode(value))
+              Right(codec.decode(valueStr))
             } catch {
               case t: Throwable => Left(s"Unable to bind from key: $key, error: ${t.getMessage}")
             }
           case _ => Left(s"Unable to bind from key: $key")
         }
       }
+
+    override def unbind(key: String, value: T): String = stringBinder.unbind(key, codec.encode(value))
+  }
+
+  implicit def pathBindableParser[T](implicit stringBinder: PathBindable[String], codec: Codec[T]): PathBindable[T] = new PathBindable[T] {
+    override def bind(key: String, value: String): Either[String, T] = {
+      val valueStr = stringBinder.bind(key, value)
+      valueStr match {
+        case Right(valueStr) =>
+          try {
+            Right(codec.decode(valueStr))
+          } catch {
+            case t: Throwable => Left(s"Unable to bind from key: $key, error: ${t.getMessage}")
+          }
+        case _ => Left(s"Unable to bind from key: $key")
+      }
+    }
 
     override def unbind(key: String, value: T): String = stringBinder.unbind(key, codec.encode(value))
   }
