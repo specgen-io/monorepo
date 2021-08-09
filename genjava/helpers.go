a4eaa7226f22f63442af721ea8ddc088882c88dc
Anastasiya <nislovskaya96@gmail.com>
2021-08-09 13:59:02 +0300

    genjava-unions: Added unions generation and changed package name

--- genjava/helpers.go
+++ genjava/helpers.go
@@ -3,6 +3,7 @@ package genjava
 import (
 	"fmt"
 	"github.com/specgen-io/spec"
+	"strings"
 )
 
 func versionedFolder(version spec.Name, folder string) string {
@@ -12,9 +13,16 @@ func versionedFolder(version spec.Name, folder string) string {
 	return folder
 }
 
-func versionedPackage(version spec.Name, packageName string) string {
+func versionedPackage(specification *spec.Spec, version spec.Name, packageName string) string {
 	if version.Source != "" {
-		return fmt.Sprintf("%s_%s", packageName, version.FlatCase())
+		return fmt.Sprintf("%s.models.%s_%s", validSpecificationName(specification), packageName, version.FlatCase())
 	}
-	return packageName
+	return fmt.Sprintf("%s.models.%s", validSpecificationName(specification), packageName)
+}
+
+func validSpecificationName(specification *spec.Spec) string {
+	if strings.Contains(specification.Name.Source, "-") {
+		return strings.ReplaceAll(specification.Name.Source, "-", "_")
+	}
+	return specification.Name.Source
 }
