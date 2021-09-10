91cd5f9f203e55d6da5adec8fd1e29f0713290bb
Anastasiya Nislovskaya <nislovskaya96@gmail.com>
2021-09-10 14:01:10 +0300

    genjava-models: Modified generation of equals, hashCode and toString (#50)

--- genjava/check_types.go
+++ genjava/check_types.go
@@ -23,11 +23,63 @@ func checkType(fieldType *spec.TypeDef, typ string) bool {
 	return true
 }
 
-func isArrayField(model *spec.NamedModel) bool {
-	for _, field := range model.Object.Fields {
-		if field.Type.Definition.Node == spec.ArrayType {
-			return true
-		}
+func isArrayField(typ *spec.TypeDef) bool {
+	if typ.Node == spec.ArrayType {
+		return true
 	}
 	return false
+}
+
+func equalsExpression(typ *spec.TypeDef, left string, right string) string {
+	return _equalsExpression(typ, left, right, true)
+}
+
+func _equalsExpression(typ *spec.TypeDef, left string, right string, canBePrimitiveType bool) string {
+	switch typ.Node {
+	case spec.PlainType:
+		return equalsExpressionPlainType(typ.Plain, left, right, canBePrimitiveType)
+	case spec.NullableType:
+		return _equalsExpression(typ.Child, left, right, false)
+	case spec.ArrayType:
+		return fmt.Sprintf(`Arrays.equals(%s, that.%s)`, left, right)
+	case spec.MapType:
+		return fmt.Sprintf(`Objects.equals(%s, that.%s)`, left, right)
+	default:
+		panic(fmt.Sprintf("Unknown type: %v", typ))
+	}
+}
+
+func equalsExpressionPlainType(typ string, left string, right string, canBePrimitiveType bool) string {
+	switch typ {
+	case spec.TypeInt32,
+		spec.TypeInt64,
+		spec.TypeBoolean:
+		if canBePrimitiveType {
+			return fmt.Sprintf(`%s == that.%s`, left, right)
+		} else {
+			return fmt.Sprintf(`Objects.equals(%s, that.%s)`, left, right)
+		}
+	case spec.TypeFloat:
+		return fmt.Sprintf(`Float.compare(that.%s, %s) == 0`, left, right)
+	case spec.TypeDouble:
+		return fmt.Sprintf(`Double.compare(that.%s, %s) == 0`, left, right)
+	case spec.TypeDecimal,
+		spec.TypeString,
+		spec.TypeUuid,
+		spec.TypeDate,
+		spec.TypeDateTime,
+		spec.TypeJson:
+		return fmt.Sprintf(`Objects.equals(%s, that.%s)`, left, right)
+	default:
+		return fmt.Sprintf(`Objects.equals(%s, that.%s)`, left, right)
+	}
+}
+
+func toStringExpression(typ *spec.TypeDef, value string) string {
+	switch typ.Node {
+	case spec.ArrayType:
+		return fmt.Sprintf(`Arrays.toString(%s)`, value)
+	default:
+		return fmt.Sprintf(`%s`, value)
+	}
 }
