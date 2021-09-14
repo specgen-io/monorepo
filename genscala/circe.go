5ad7a5d8d1bafca6243e16e5adfc776b7c982ea2
Vladimir Sapronov <vladimir@specgen.io>
2021-09-14 01:24:43 -0400

    genscala: models circe added oneOf discriminator support

--- genscala/circe.go
+++ genscala/circe.go
@@ -14,6 +14,8 @@ func GenerateCirceModels(spec *spec.Spec, packageName string, outPath string) []
 		versionFile := generateCirceModels(&version, packageName, outPath)
 		files = append(files, *versionFile)
 	}
+	taggedUnion := generateTaggedUnion("spec", filepath.Join(outPath, "TaggedUnion.scala"))
+	files = append(files, *taggedUnion)
 	return files
 }
 
@@ -32,6 +34,7 @@ func generateCirceModels(version *spec.Version, packageName string, outPath stri
 	w.Line(`import io.circe.Codec`)
 	w.Line(`import io.circe.generic.extras.{Configuration, JsonKey}`)
 	w.Line(`import io.circe.generic.extras.semiauto.{deriveConfiguredCodec, deriveUnwrappedCodec}`)
+	w.Line(`import spec.taggedunion._`)
 
 	for _, model := range version.ResolvedModels {
 		w.EmptyLine()
@@ -87,13 +90,19 @@ func generateCirceUnionModel(w *gen.Writer, model *spec.NamedModel, packageName 
 	for _, item := range model.OneOf.Items {
 		w.Line(`  case class %s(data: %s.%s) extends %s`, item.Name.PascalCase(), packageName, ScalaType(&item.Type.Definition), traitName)
 	}
-	w.Line(`  implicit val config = Configuration.default.withSnakeCaseConstructorNames`)
-	w.Line(`  implicit val codec: Codec[%s] = deriveConfiguredCodec`, traitName)
-
+	w.EmptyLine()
 	for _, item := range model.OneOf.Items {
-		itemTypeName := model.Name.PascalCase() + "." + item.Name.PascalCase()
-		w.Line(`  implicit val codec%s: Codec[%s] = deriveUnwrappedCodec`, item.Name.PascalCase(), itemTypeName)
+		caseName := item.Name.PascalCase()
+		w.Line(`  implicit val codec%s: Codec[%s] = deriveUnwrappedCodec`, caseName, caseName)
+		w.Line(`  implicit val tag%s: Tag[%s] = Tag("%s")`, caseName, caseName, item.Name.Source)
 	}
+	w.EmptyLine()
+	unionConfig := "Config.wrapped"
+	if model.OneOf.Discriminator != nil {
+		unionConfig = fmt.Sprintf(`Config.discriminator("%s")`, *model.OneOf.Discriminator)
+	}
+	w.Line(`  implicit val config: Config[%s] = %s`, traitName, unionConfig)
+	w.Line(`  implicit val codec: Codec[%s] = unionCodec`, traitName)
 	w.Line(`}`)
 }
 
@@ -127,6 +136,144 @@ object Jsoner {
       Printer.spaces2.copy(dropNullValues = true).print(value.asJson)
     } else {
       Printer.noSpaces.copy(dropNullValues = true).print(value.asJson)
+    }
+  }
+}`
+	code, _ = gen.ExecuteTemplate(code, struct{ PackageName string }{packageName})
+	return &gen.TextFile{path, strings.TrimSpace(code)}
+}
+
+
+func generateTaggedUnion(packageName string, path string) *gen.TextFile {
+	code := `
+package [[.PackageName]]
+
+object taggedunion {
+  import io.circe._
+  import shapeless.{:+:, CNil, Coproduct, Generic, Inl, Inr, Lazy}
+
+  implicit val cnilEncoder: Encoder[CNil] =
+    Encoder.instance(cnil => throw new Exception("Inconceivable!"))
+
+  implicit val cnilDecoder: Decoder[CNil] =
+    Decoder.instance(a => Left(DecodingFailure("Discriminator field or wrapped object not recognized", a.history)))
+
+  implicit def coproductEncoder[H, T <: Coproduct](
+    implicit hEncoder: Lazy[Encoder[H]],
+    tag: Lazy[Tag[H]],
+    config: Lazy[Config[H]],
+    tEncoder: Encoder[T]
+  ): Encoder[H :+: T] = Encoder.instance{
+    case Inl(head) =>
+      encodeWithConfig(config.value, tag.value, hEncoder.value)(head)
+    case Inr(tail) => tEncoder(tail)
+  }
+
+  implicit def coproductDecoder[H, T <: Coproduct](
+    implicit hDecoder: Lazy[Decoder[H]],
+    tag: Lazy[Tag[H]],
+    config: Lazy[Config[H]],
+    tDecoder: Decoder[T]
+  ): Decoder[H :+: T] = Decoder.instance{ cursor =>
+    decodeWithConfig(cursor, tag.value, config.value, hDecoder) match {
+      case Left(NotMatch)   => tDecoder.tryDecode(cursor).map(Inr(_))
+      case Right(value)     => Right(Inl[H, T](value))
+      case Left(InternalParsingError(circeError))           => Left[DecodingFailure, H :+: T](circeError)
+      case Left(DiscriminatorFieldCorrupted(circeError))    => Left[DecodingFailure, H :+: T](circeError)
+    }
+  }
+
+  def unionEncoder[A, R](implicit gen: Generic.Aux[A, R], enc: Encoder[R]): Encoder[A] = {
+    Encoder.instance(a => enc(gen.to(a)))
+  }
+
+  def unionDecoder[A, R](implicit gen: Generic.Aux[A, R], dec: Decoder[R]): Decoder[A] = {
+    Decoder.instance(a => dec(a).map(gen.from))
+  }
+
+  def unionCodec[A, R](
+    implicit
+    gen: Generic.Aux[A, R],
+    enc: Encoder[R],
+    dec: Decoder[R]
+  ): Codec[A] = new Codec[A] {
+    override def apply(a: A): Json = enc(gen.to(a))
+    override def apply(c: HCursor): Decoder.Result[A] = dec(c).map(gen.from)
+  }
+
+  trait Tag[A]{
+    def name: String
+  }
+
+  object Tag{
+    def apply[A](string: String): Tag[A] =
+      new Tag[A] {
+        override def name: String = string
+      }
+  }
+
+  sealed trait CoproductConfig[-A]
+  case class Discriminator[A](fieldName: String) extends CoproductConfig[A]
+  case class WrappedObject[A]() extends CoproductConfig[A]
+
+  trait Config[-A]{
+    def coproduct: CoproductConfig[A]
+  }
+
+  object Config{
+    def discriminator[A](fieldName: String) = new Config[A]{
+      def coproduct: CoproductConfig[A] = Discriminator(fieldName)
+    }
+
+    def wrapped[A] = new Config[A]{
+      def coproduct: CoproductConfig[A] = WrappedObject()
+    }
+  }
+
+  def encodeWithConfig[A, H <: A](config: Config[A], tag: Tag[A], encoder: Encoder[H]) = Encoder.instance[H]{ value =>
+    config.coproduct match {
+      case discriminator: Discriminator[H] =>
+        encoder(value)
+          .mapObject(js =>
+            js.+:(discriminator.fieldName, Json.fromString(tag.name))
+          )
+      case _: WrappedObject[H] =>
+        Json.obj((tag.name, encoder(value)))
+    }
+  }
+
+  sealed trait DecodingError
+  case object NotMatch extends DecodingError
+  case class InternalParsingError(circeError: DecodingFailure) extends DecodingError
+  case class DiscriminatorFieldCorrupted(circeError: DecodingFailure) extends DecodingError
+
+  def decodeWithConfig[H](cursor: HCursor, tag: Tag[H], config: Config[H], hDecoder: Lazy[Decoder[H]]): Either[DecodingError, H] = {
+    config.coproduct match {
+      case Discriminator(fieldName) =>
+        cursor.downField(fieldName).as[String] match {
+          case Left(value)  =>
+            Left(DiscriminatorFieldCorrupted(value))
+
+          case Right(discriminator) =>
+            if(discriminator == tag.name){
+              hDecoder.value.tryDecode(cursor) match {
+                case Left(value)  => Left(InternalParsingError(value))
+                case Right(value) => Right(value)
+              }
+            } else {
+              Left(NotMatch)
+            }
+        }
+
+      case WrappedObject() => {
+        hDecoder.value.tryDecode(cursor.downField(tag.name)) match {
+          case Left(value)  =>
+            Left(NotMatch)
+
+          case Right(discriminator) =>
+            Right(discriminator)
+        }
+      }
     }
   }
 }`
