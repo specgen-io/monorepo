09872c45bd8993cd3dc71c56a73a07b39afaaf32
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-09-21 20:22:47 -0400

    gengo: Removed unused function

--- gengo/imports.go
+++ gengo/imports.go
@@ -7,22 +7,6 @@ import (
 	"sort"
 )
 
-func generateVersionImports(version *spec.Version, imports []string) []string {
-	if versionHasType(version, spec.TypeDate) {
-		imports = append(imports, `"cloud.google.com/go/civil"`)
-	}
-	if versionHasType(version, spec.TypeJson) {
-		imports = append(imports, `"encoding/json"`)
-	}
-	if versionHasType(version, spec.TypeUuid) {
-		imports = append(imports, `"github.com/google/uuid"`)
-	}
-	if versionHasType(version, spec.TypeDecimal) {
-		imports = append(imports, `"github.com/shopspring/decimal"`)
-	}
-	return imports
-}
-
 func generateApiImports(api *spec.Api, imports []string) []string {
 	if apiHasType(api, spec.TypeDate) {
 		imports = append(imports, `"cloud.google.com/go/civil"`)
