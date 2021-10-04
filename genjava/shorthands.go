f743f3ddbbcab95ea75c2cb257487d375df179c0
Anastasiya Nislovskaya <nislovskaya96@gmail.com>
2021-10-04 13:33:05 +0300

    Genjava: Added Spring service generation (#55)
    * genjava: Added Spring service generation
    
    * genjava: Renamed variables
    
    * genjava: Added Spring service generation
    
    * genjava: Renamed variables
    
    * genjava: Service generation refactoring

--- genjava/shorthands.go
+++ genjava/shorthands.go
@@ -1,8 +1,11 @@
 package genjava
 
 import (
+	"github.com/pinzolo/casee"
 	"strings"
 )
+
+var ToPascalCase = casee.ToPascalCase
 
 func JoinParams(params []string) string {
 	return strings.Join(params, ", ")
