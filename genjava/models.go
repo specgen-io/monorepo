6a0b065804f244dd17daa694d52b5b033d89d9e1
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-10-09 13:58:37 -0400

    genjava: Simplified Jsoner

--- genjava/models.go
+++ genjava/models.go
@@ -38,18 +38,16 @@ import java.io.*;
 
 public class Jsoner {
 
+	public static void setupObjectMapper(ObjectMapper objectMapper) {
+		objectMapper.registerModule(new JavaTimeModule()).configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+	}
+
 	public static <T> String serialize(ObjectMapper objectMapper, T data) throws IOException {
-		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-		objectMapper.registerModule(new JavaTimeModule())
-			.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-		
-		objectMapper.writeValue(byteArrayOutputStream, data);
-		return byteArrayOutputStream.toString();
+		return objectMapper.writeValueAsString(data);
 	}
 
 	public static <T> T deserialize(ObjectMapper objectMapper, String jsonStr, Class<T> tClass) throws IOException {
-		InputStream inputStream = new ByteArrayInputStream(jsonStr.getBytes());
-		return objectMapper.readValue(inputStream, tClass);
+		return objectMapper.readValue(jsonStr, tClass);
 	}
 }
 `
