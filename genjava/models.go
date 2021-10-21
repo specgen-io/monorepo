9566b2fb9db70b6c580be1cc577af59d928be549
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-10-21 00:31:19 -0400

    genjava: Do not include nulls

--- genjava/models.go
+++ genjava/models.go
@@ -31,14 +31,16 @@ func generateJson(thePackage Module) *gen.TextFile {
 	code := `
 package [[.PackageName]];
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.datatype.jsr310.*;
 
-import java.io.*;
-
 public class Json {
 	public static void setupObjectMapper(ObjectMapper objectMapper) {
-		objectMapper.registerModule(new JavaTimeModule()).configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		objectMapper
+				.registerModule(new JavaTimeModule())
+				.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+				.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 	}
 }
 `
