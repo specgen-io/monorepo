d010c601cae94c177739265d7ec8d1cedf728a4b
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-12-07 11:54:34 -0500

    conopenapi: Added kin-openapi

---
+++ conopenapi/type.go
@@ -0,0 +1,11 @@
+package conopenapi
+
+import (
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/specgen-io/specgen/v2/spec"
+)
+
+func SchemaType(schema *openapi3.Schema) *spec.TypeDef {
+	//schema.DecodeWith()
+	return nil
+}
