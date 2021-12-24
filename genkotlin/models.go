8bd963d5077dd8a31671ea4d648b684597e3d89f
Nislovskaya96 <nislovskaya96@gmail.com>
2021-12-24 17:43:03 +0300

    genkotlin: Added OkHttp Kotlin client generation

--- genkotlin/models.go
+++ genkotlin/models.go
@@ -35,7 +35,7 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.*
 import com.fasterxml.jackson.datatype.jsr310.*
 
-fun setupObjectMapper(objectMapper: ObjectMapper): ObjectMapper  {
+fun setupObjectMapper(objectMapper: ObjectMapper): ObjectMapper {
     objectMapper
         .registerModule(JavaTimeModule())
         .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
