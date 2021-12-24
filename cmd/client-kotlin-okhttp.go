1ab52c0c5c88153e736a56e2db6faecd5c4712a5
Nislovskaya96 <nislovskaya96@gmail.com>
2021-12-24 17:43:03 +0300

    genkotlin: Added OkHttp Kotlin client generation

---
+++ cmd/client-kotlin-okhttp.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"github.com/specgen-io/specgen/v2/fail"
+	"github.com/specgen-io/specgen/v2/genkotlin"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	cmdClientKotlinOkHttp.Flags().String(SpecFile, "", SpecFileDescription)
+	cmdClientKotlinOkHttp.Flags().String(PackageName, "", PackageNameDescription)
+	cmdClientKotlinOkHttp.Flags().String(GeneratePath, "", GeneratePathDescription)
+
+	cmdClientKotlinOkHttp.MarkFlagRequired(SpecFile)
+	cmdClientKotlinOkHttp.MarkFlagRequired(PackageName)
+	cmdClientKotlinOkHttp.MarkFlagRequired(GeneratePath)
+
+	rootCmd.AddCommand(cmdClientKotlinOkHttp)
+}
+
+var cmdClientKotlinOkHttp = &cobra.Command{
+	Use:   "client-kotlin-okhttp",
+	Short: "Generate OkHttp Kotlin client source code",
+	Run: func(cmd *cobra.Command, args []string) {
+		specFile, err := cmd.Flags().GetString(SpecFile)
+		fail.IfError(err)
+
+		packageName, err := cmd.Flags().GetString(PackageName)
+		fail.IfError(err)
+
+		generatePath, err := cmd.Flags().GetString(GeneratePath)
+		fail.IfError(err)
+
+		specification := readSpecFile(specFile)
+
+		err = genkotlin.GenerateClient(specification, packageName, generatePath)
+		fail.IfErrorF(err, "Failed to generate client code")
+	},
+}
