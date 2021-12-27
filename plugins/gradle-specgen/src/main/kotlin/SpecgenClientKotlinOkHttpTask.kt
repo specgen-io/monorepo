a6b42a125573ba479e204a4a4076dc4cb74f40e7
Nislovskaya96 <nislovskaya96@gmail.com>
2021-12-27 16:55:43 +0300

    gradle-plugin: Added client-kotlin-okhttp task

---
+++ plugins/gradle-specgen/src/main/kotlin/SpecgenClientKotlinOkHttpTask.kt
@@ -0,0 +1,47 @@
+package io.specgen.gradle
+
+import org.gradle.api.Project
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.*
+import org.gradle.kotlin.dsl.*
+import java.io.File
+import javax.inject.Inject
+
+public open class ClientKotlinOkHttpConfig @Inject constructor(project: Project) {
+    @OutputDirectory
+    public val outputDirectory: Property<File> =
+        project.objects.property<File>().convention(project.buildDir.resolve("generated-src/specgen"))
+
+    @InputFile
+    @PathSensitive(value = PathSensitivity.RELATIVE)
+    public val specFile: Property<File> =
+        project.objects.property<File>().convention(project.projectDir.resolve("spec.yaml"))
+
+    @Input
+    @Optional
+    public val packageName: Property<String> = project.objects.property()
+}
+
+public open class SpecgenClientKotlinOkHttpTask public constructor() : SpecgenBaseTask() {
+    @TaskAction
+    public fun execute() {
+        val extension = project.extensions.findByType<SpecgenPluginExtension>()
+        // TODO: Check if there are nulls below
+        val config = extension!!.configClientKotlinOkHttp!!
+
+        val commandlineArgs = mutableListOf(
+            "client-kotlin-okhttp",
+            "--spec-file",
+            config.specFile.get().absolutePath,
+            "--generate-path",
+            config.outputDirectory.get().absolutePath,
+        )
+
+        if (config.packageName.isPresent) {
+            commandlineArgs.add("--package-name")
+            commandlineArgs.add(config.packageName.get())
+        }
+
+        runSpecgen(commandlineArgs)
+    }
+}
