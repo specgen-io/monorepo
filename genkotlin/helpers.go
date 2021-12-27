3aaba1a07690c7dc40abba9de083d436773bf7c3
Anastasiya Nislovskaya <anastasiya@specgen.io>
2021-12-27 21:14:38 +0300

    genkotlin: Added OkHttp Kotlin client generation (#91)
    * genkotlin: Added OkHttp Kotlin client generation
    
    * genkotlin: Added OkHttp Kotlin client generation
    
    * Go empty (#92)
    
    * gengo: fix for empty type
    
    * gengo: fix for service compilation
    
    * gengo: fix for empty type generation
    
    * gengo: fix for empty type in clients
    
    * gengo: fix for empty type in models
    
    * gengo: models validation is implemented
    
    * gengo: fix for api referencing empty type
    
    * gengo: no empty type in models
    
    * gengo: removed service impl postfixes
    
    * gengo: refactored commands to work with sources
    
    * genjava: refactored commands to work with sources
    
    * genkotlin: Added OkHttp Kotlin client generation
    
    Co-authored-by: Vladimir Sapronov <vladimir.sapronov@gmail.com>

---
+++ genkotlin/helpers.go
@@ -0,0 +1,21 @@
+package genkotlin
+
+import (
+	"fmt"
+	"github.com/specgen-io/specgen/v2/spec"
+)
+
+func serviceResponseInterfaceName(operation *spec.NamedOperation) string {
+	return fmt.Sprintf(`%sResponse`, operation.Name.PascalCase())
+}
+
+func clientName(api *spec.Api) string {
+	return fmt.Sprintf(`%sClient`, api.Name.PascalCase())
+}
+
+func addBuilderParam(param *spec.NamedParam) string {
+	if param.Type.Definition.IsNullable() {
+		return fmt.Sprintf(`%s!!`, param.Name.CamelCase())
+	}
+	return param.Name.CamelCase()
+}
