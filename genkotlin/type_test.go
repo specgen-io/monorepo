49fd1d72337c9d1fef77758e00c81861a3af403b
Nislovskaya96 <nislovskaya96@gmail.com>
2021-12-28 14:32:08 +0300

    Revert changes

--- genkotlin/type_test.go
+++ genkotlin/type_test.go
@@ -69,7 +69,7 @@ func TestNullableType(t *testing.T) {
 func TestArrayType(t *testing.T) {
 	typ := spec.Array(spec.Plain(spec.TypeString))
 	javaType := KotlinType(typ)
-	assert.Equal(t, javaType, "Array<String>")
+	assert.Equal(t, javaType, "List<String>")
 }
 
 func TestMapType(t *testing.T) {
@@ -81,7 +81,7 @@ func TestMapType(t *testing.T) {
 func TestComplexType(t *testing.T) {
 	typ := spec.Array(spec.Nullable(spec.Plain(spec.TypeBoolean)))
 	javaType := KotlinType(typ)
-	assert.Equal(t, javaType, "Array<Boolean?>")
+	assert.Equal(t, javaType, "List<Boolean?>")
 }
 
 func TestMapScalarType(t *testing.T) {
