6ea70b8ca3f2fb811a134f54fdfcb331320859df
Nislovskaya96 <nislovskaya96@gmail.com>
2021-12-30 17:05:27 +0300

    genkotlin: Fixed hashCode and builders generation

--- genkotlin/models.go
+++ genkotlin/models.go
@@ -132,28 +132,32 @@ func generateObjectModelMethods(w *gen.Writer, model *spec.NamedModel) {
 	var arrayFieldCount, nonArrayFieldCount int
 	for _, field := range model.Object.Fields {
 		if _isKotlinArrayType(&field.Type.Definition) {
-			hashCodeArrayParams = append(hashCodeArrayParams, field.Name.CamelCase())
+			hashCodeArrayParams = append(hashCodeArrayParams, fmt.Sprintf(`%s.contentHashCode()`, field.Name.CamelCase()))
 			arrayFieldCount++
 		} else {
-			hashCodeNonArrayParams = append(hashCodeNonArrayParams, field.Name.CamelCase())
+			if field.Type.Definition.IsNullable() {
+				hashCodeNonArrayParams = append(hashCodeNonArrayParams, fmt.Sprintf(`(%s?.hashCode() ?: 0)`, field.Name.CamelCase()))
+			} else {
+				hashCodeNonArrayParams = append(hashCodeNonArrayParams, fmt.Sprintf(`%s.hashCode()`, field.Name.CamelCase()))
+			}
 			nonArrayFieldCount++
 		}
 	}
 	if arrayFieldCount == 1 && nonArrayFieldCount == 0 {
-		w.Line(`  return %s.contentHashCode()`, hashCodeArrayParams[0])
+		w.Line(`  return %s`, hashCodeArrayParams[0])
 	} else if arrayFieldCount > 1 && nonArrayFieldCount == 0 {
-		w.Line(`  var result = %s.contentHashCode()`, hashCodeArrayParams[0])
+		w.Line(`  var result = %s`, hashCodeArrayParams[0])
 		for _, param := range hashCodeArrayParams[1:] {
-			w.Line(`  result = 31 * result + %s.contentHashCode()`, param)
+			w.Line(`  result = 31 * result + %s`, param)
 		}
 		w.Line(`  return result`)
 	} else if arrayFieldCount > 0 && nonArrayFieldCount > 0 {
-		w.Line(`  var result = %s.contentHashCode()`, hashCodeArrayParams[0])
+		w.Line(`  var result = %s`, hashCodeArrayParams[0])
 		for _, param := range hashCodeArrayParams[1:] {
-			w.Line(`  result = 31 * result + %s.contentHashCode()`, param)
+			w.Line(`  result = 31 * result + %s`, param)
 		}
 		for _, param := range hashCodeNonArrayParams {
-			w.Line(`  result = 31 * result + %s.hashCode()`, param)
+			w.Line(`  result = 31 * result + %s`, param)
 		}
 		w.Line(`  return result`)
 	}
