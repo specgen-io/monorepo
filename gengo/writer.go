b501e7e2f15359d9f36e9dd0cc3fd79fe00e40b1
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-12-31 23:28:44 -0500

    gengo: fixed unions and aligned structs definitions

--- gengo/writer.go
+++ gengo/writer.go
@@ -1,7 +1,42 @@
 package gengo
 
-import "github.com/specgen-io/specgen/v2/sources"
+import (
+	"github.com/specgen-io/specgen/v2/sources"
+	"strings"
+)
 
 func NewGoWriter() *sources.Writer {
 	return sources.NewWriter("\t", 2)
+}
+
+func colWidth(lines [][]string, colIndex int) int {
+	width := 0
+	for _, line := range lines {
+		rowWidth := len(line[colIndex])
+		if rowWidth > width {
+			width = rowWidth
+		}
+	}
+	return width
+}
+
+func space(value string, length int, spacer string) string {
+	return value + strings.Repeat(spacer, length-len(value))
+}
+
+func WriteAlignedLines(w *sources.Writer, lines [][]string) {
+	widths := make([]int, len(lines[0]))
+	for colIndex, _ := range lines[0] {
+		widths[colIndex] = colWidth(lines, colIndex)
+	}
+	for _, line := range lines {
+		lineStr := ""
+		for colIndex, cell := range line {
+			lineStr += cell
+			if colIndex != len(line)-1 {
+				lineStr += strings.Repeat(" ", widths[colIndex]-len(cell)) + " "
+			}
+		}
+		w.Line(lineStr)
+	}
 }
