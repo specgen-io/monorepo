323d8604238f1ed34b20783fa20e55ff91623e86
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2022-01-10 14:12:40 -0500

    conopenapi: half-working spec generation

--- conopenapi/openapi.go
+++ conopenapi/openapi.go
@@ -10,79 +10,23 @@ func ConvertFromOpenapi(inFile, outFile string) error {
 	if err != nil {
 		return err
 	}
-	specification := generateSpec(doc)
+	specification := convertSpec(doc)
 	err = spec.WriteSpecFile(specification, outFile)
 	return err
 }
 
-func generateSpec(doc *openapi3.T) *spec.Spec {
-	api := Api{"default", []PathItem{}}
-	for path, pathItem := range doc.Paths {
-		for method, o := range pathItem.Operations() {
-			pathItem := PathItem{path, method, o}
-			api.Items = append(api.Items, pathItem)
-		}
-	}
-
-	specApi := generateSpecApi(&api)
-
-	apis := spec.Apis{nil, []spec.Api{*specApi}, nil}
-	version := spec.Version{spec.Name{"", nil}, spec.VersionSpecification{apis, []spec.NamedModel{}}, []*spec.NamedModel{}}
-
-	meta := spec.Meta{"2.1", spec.Name{doc.Info.Title, nil}, &doc.Info.Title, &doc.Info.Description, doc.Info.Version}
-	specification := spec.Spec{meta, []spec.Version{version}}
+func convertSpec(doc *openapi3.T) *spec.Spec {
+	meta := spec.Meta{spec.SpecVersion, name(doc.Info.Title), &doc.Info.Title, &doc.Info.Description, doc.Info.Version}
+	versions := convertVersions(doc)
+	specification := spec.Spec{meta, versions}
 	return &specification
 }
 
-func generateSpecApi(api *Api) *spec.Api {
-	operations := []spec.NamedOperation{}
-	for _, pathItem := range api.Items {
-		operation := generateSpecOperation(&pathItem)
-		operations = append(operations, spec.NamedOperation{spec.Name{pathItem.Operation.OperationID, nil}, *operation, nil})
-	}
-	return &spec.Api{spec.Name{api.Name, nil}, operations, nil}
-}
-
-func generateSpecOperation(pathItem *PathItem) *spec.Operation {
-	endpoint := spec.Endpoint{pathItem.Method, pathItem.Path, []spec.NamedParam{}, nil}
-	var description *string = nil
-	if pathItem.Operation.Description != "" {
-		description = &pathItem.Operation.Description
-	}
-	operation := spec.Operation{
-		endpoint,
-		description,
-		[]spec.NamedParam{},
-		[]spec.NamedParam{},
-		getBody(pathItem.Operation.RequestBody),
-		[]spec.NamedResponse{},
+func convertVersions(doc *openapi3.T) []spec.Version {
+	version := spec.Version{
+		name(""),
+		spec.VersionSpecification{convertApis(doc), convertModels(doc.Components.Schemas)},
 		nil,
 	}
-	return &operation
-}
-
-func getBody(body *openapi3.RequestBodyRef) *spec.Definition {
-	if body == nil {
-		return nil // this is fair - no body means nil definition
-	}
-	if body.Value == nil {
-		return nil //TODO: not sure in this - what if ref is specified here
-	}
-	media := body.Value.Content.Get("application/json")
-	if media == nil {
-		return nil
-	}
-	definition := spec.Definition{spec.Type{*SpecTypeFromSchemaRef(media.Schema), nil}, &body.Value.Description, nil}
-	return &definition
-}
-
-type Api struct {
-	Name  string
-	Items []PathItem
-}
-
-type PathItem struct {
-	Path      string
-	Method    string
-	Operation *openapi3.Operation
+	return []spec.Version{version}
 }
