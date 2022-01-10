965d526db8af189a23435ec9170fc2a7c8d035f1
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2022-01-10 14:12:40 -0500

    conopenapi: half-working spec generation

---
+++ conopenapi/utils.go
@@ -0,0 +1,9 @@
+package conopenapi
+
+import "github.com/specgen-io/specgen/v2/spec"
+
+func name(source string) spec.Name {
+	return spec.Name{source, nil}
+}
+
+var emptyType = spec.Type{*spec.Plain(spec.TypeEmpty), nil}
