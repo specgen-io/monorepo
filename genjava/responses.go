00b3d9ba279eb205dd0d88ffe11447aba2aceaba
Vladimir Sapronov <vladimir@specgen.io>
2022-01-13 13:14:42 -0500

    genjava: some files renames moved responses code into separate file

--- genjava/responses.go
+++ genjava/responses.go
@@ -6,47 +6,6 @@ import (
 	"github.com/specgen-io/specgen/v2/spec"
 )
 
-func (g *Generator) generateServicesInterfaces(version *spec.Version, thePackage Module, modelsVersionPackage Module) []sources.CodeFile {
-	files := []sources.CodeFile{}
-	for _, api := range version.Http.Apis {
-		apiPackage := thePackage.Subpackage(api.Name.SnakeCase())
-		files = append(files, g.generateInterface(&api, apiPackage, modelsVersionPackage)...)
-	}
-	return files
-}
-
-func (g *Generator) generateInterface(api *spec.Api, apiPackage Module, modelsVersionPackage Module) []sources.CodeFile {
-	files := []sources.CodeFile{}
-
-	w := NewJavaWriter()
-	w.Line(`package %s;`, apiPackage.PackageName)
-	w.EmptyLine()
-	w.Line(`import java.math.BigDecimal;`)
-	w.Line(`import java.time.*;`)
-	w.Line(`import java.util.*;`)
-	w.EmptyLine()
-	w.Line(`import %s;`, modelsVersionPackage.PackageStar)
-	w.EmptyLine()
-	w.Line(`public interface %s {`, serviceInterfaceName(api))
-	for _, operation := range api.Operations {
-		w.Line(`  %s;`, g.generateResponsesSignatures(&operation))
-	}
-	w.Line(`}`)
-
-	for _, operation := range api.Operations {
-		if len(operation.Responses) > 1 {
-			files = append(files, g.generateResponseInterface(&operation, apiPackage, modelsVersionPackage)...)
-		}
-	}
-
-	files = append(files, sources.CodeFile{
-		Path:    apiPackage.GetPath(fmt.Sprintf("%s.java", serviceInterfaceName(api))),
-		Content: w.String(),
-	})
-
-	return files
-}
-
 func (g *Generator) generateResponsesSignatures(operation *spec.NamedOperation) string {
 	if len(operation.Responses) == 1 {
 		for _, response := range operation.Responses {
