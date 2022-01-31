6ea6ed5f93dcba7d2be9a57b24989b75aa2f1ab0
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2022-01-31 15:53:26 -0500

    genjava: removed jsonlib name from client generator

--- genkotlin/client/generator.go
+++ genkotlin/client/generator.go
@@ -6,14 +6,12 @@ import (
 )
 
 type Generator struct {
-	Jsonlib string
-	Types   *types.Types
-	Models  models.Generator
+	Types  *types.Types
+	Models models.Generator
 }
 
 func NewGenerator(jsonlib string) *Generator {
 	return &Generator{
-		jsonlib,
 		models.NewTypes(jsonlib),
 		models.NewGenerator(jsonlib),
 	}
