2023aab7a8dcbc615b78084b0ed2bed0eae15264
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2022-02-12 12:51:20 -0500

    genjava: Fixed moshi unwrap adapter to use getter instead of field

--- genjava/models/moshi.go
+++ genjava/models/moshi.go
@@ -715,6 +715,7 @@ package [[.PackageName]];
 
 import com.squareup.moshi.*;
 import org.jetbrains.annotations.Nullable;
+
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
@@ -736,29 +737,40 @@ public final class UnwrapFieldAdapterFactory<T> implements JsonAdapter.Factory {
 
         final Field[] fields = this.type.getDeclaredFields();
         if (fields.length != 1) {
-            throw new RuntimeException("Type "+type.getTypeName()+" has "+fields.length+" fields, unwrap adapter can be used only with single-field types");
+            throw new RuntimeException("Type "+type.getTypeName()+" has "+fields.length+" fields, unwrap adapter can be used only with single-field classes");
         }
         var field = fields[0];
+
+        var fieldName = field.getName();
+        var getterName = "get" + fieldName.substring(0,1).toUpperCase() + fieldName.substring(1).toLowerCase();
+
+        Method getter;
+        try {
+            getter = this.type.getDeclaredMethod(getterName);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("Type "+type.getTypeName()+" field "+fieldName+" does not have getter method "+ field.getType().getName()+", it's required for unwrap adapter", e);
+        }
 
         Constructor<T> constructor;
         try {
             constructor = this.type.getDeclaredConstructor(field.getType());
         } catch (NoSuchMethodException e) {
-            throw new RuntimeException("Type "+type.getTypeName()+" does not have constructor with single parameter of type "+ field.getType().getName()+", it's required for unwrap adapter");
+            throw new RuntimeException("Type "+type.getTypeName()+" does not have constructor with single parameter of type "+ field.getType().getName()+", it's required for unwrap adapter", e);
         }
 
         JsonAdapter<?> fieldAdapter = moshi.adapter(field.getType());
-        return new UnwrapFieldAdapter(constructor, field, fieldAdapter);
+
+        return new UnwrapFieldAdapter(constructor, getter, fieldAdapter);
     }
 
     public class UnwrapFieldAdapter<O, I> extends JsonAdapter<Object> {
         private Constructor<O> constructor;
-        private Field field;
+        private Method getter;
         private JsonAdapter<I> fieldAdapter;
 
-        public UnwrapFieldAdapter(Constructor<O> constructor, Field field, JsonAdapter<I> fieldAdapter) {
+        public UnwrapFieldAdapter(Constructor<O> constructor, Method getter, JsonAdapter<I> fieldAdapter) {
             this.constructor = constructor;
-            this.field = field;
+            this.getter = getter;
             this.fieldAdapter = fieldAdapter;
         }
 
@@ -774,18 +786,22 @@ public final class UnwrapFieldAdapterFactory<T> implements JsonAdapter.Factory {
 
         @Override
         public void toJson(JsonWriter writer, Object value) throws IOException {
-            I fieldValue;
-            try {
-                fieldValue = (I)field.get(value);
-            } catch (IllegalAccessException e) {
-                throw new IOException("Failed to get value of field "+field.getName(), e);
+            if (value == null) {
+                fieldAdapter.toJson(writer, null);
+            } else {
+                I fieldValue;
+                try {
+                    fieldValue = (I) getter.invoke(value);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new IOException("Failed to call method "+ getter.getName(), e);
+                }
+                fieldAdapter.toJson(writer, fieldValue);
             }
-            fieldAdapter.toJson(writer, fieldValue);
         }
 
         @Override
         public String toString() {
-            return "UnwrapFieldAdapter(" + field.getName() + ")";
+            return "UnwrapFieldAdapter(" + getter.getName() + ")";
         }
     }
 }
