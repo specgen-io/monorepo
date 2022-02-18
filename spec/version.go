9dfde3dcf137bd324d06e745f18b50459b82afae
Vladimir Sapronov <vladimir@specgen.io>
2022-02-18 13:58:03 -0500

    spec: Better messages for format conversions

--- spec/version.go
+++ spec/version.go
@@ -10,51 +10,51 @@ import "gopkg.in/specgen-io/yaml.v3"
 
 var SpecVersion = "2.1"
 
-func getSpecVersion(data []byte) (*string, error) {
+func getSpecVersion(data []byte) (*string, *yaml.Node, error) {
 	var node yaml.Node
 	decoder := yaml.NewDecoder(bytes.NewReader(data))
 	err := decoder.Decode(&node)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	rootNode := node.Content[0]
 
-	specVersion, err := decodeStringOptional(rootNode, "spec")
+	specVersion, specNode, err := decodeStringNodeOptional(rootNode, "spec")
 	if err != nil {
-		return nil, err
+		return nil, specNode, err
 	}
 	if specVersion != nil {
-		return specVersion, nil
+		return specVersion, specNode, nil
 	}
 
-	specVersion, err = decodeStringOptional(rootNode, "idl_version")
+	specVersion, specNode, err = decodeStringNodeOptional(rootNode, "idl_version")
 	if err != nil {
-		return nil, err
+		return nil, specNode, err
 	}
 	if specVersion != nil {
-		return specVersion, nil
+		return specVersion, specNode, nil
 	}
 
-	return nil, errors.New(`can't find "spec" or legacy "idl_version" containing spec version`)
+	return nil, nil, errors.New(`can't find "spec" or legacy "idl_version" containing spec version`)
 }
 
 func checkSpecVersion(data []byte) ([]byte, *Messages, error) {
 	messages := NewMessages()
-	specVersion, err := getSpecVersion(data)
+	specVersion, versionNode, err := getSpecVersion(data)
 	if err != nil {
-		messages.Add(convertYamlError(err))
+		messages.Add(convertYamlError(err, versionNode))
 		return nil, messages, errors.New("failed to read specification")
 	}
 
 	if *specVersion != SpecVersion {
-		messages.Add(Warning("warning: unexpected spec format version: %s; please format you spec to format %s", *specVersion, SpecVersion))
+		messages.Add(Warning("warning: unexpected spec format version: %s; please format you spec to format %s", *specVersion, SpecVersion).At(locationFromNode(versionNode)))
 		messages.Add(Info("will try to convert spec to format %s on the fly...", SpecVersion))
 
 		convertedData, err := old.FormatSpec(data, SpecVersion)
 		if err != nil {
-			messages.Add(Error("tried to convert specification - %s", err.Error()))
-			return nil, messages, errors.New(fmt.Sprintf(`unexpected spec version, expected: %s, found: %s, conversion failed: %s`, SpecVersion, *specVersion, err.Error()))
+			messages.Add(Error(`format conversion failed from: %s to: %s with error: %s`, SpecVersion, *specVersion, err.Error()).At(locationFromNode(versionNode)))
+			return nil, messages, errors.New(fmt.Sprintf(`failed to convert specification to format %s`, SpecVersion))
 		}
 		messages.Add(Info("convert spec to format %s succeeded", SpecVersion))
 		return convertedData, messages, nil
