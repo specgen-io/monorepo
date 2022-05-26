31f2aab8ee1a17a5b0550804b1737cae851677b1
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2022-05-26 13:11:58 -0400

    gen/golang: Extracted respond functions

---
+++ gen/golang/service/responses.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	"fmt"
+	"github.com/specgen-io/specgen/v2/gen/golang/module"
+	"github.com/specgen-io/specgen/v2/sources"
+	"strings"
+)
+
+func respondJson(logFields, resVar, statusCode, dataVar string) string {
+	return fmt.Sprintf(`respondJson(%s, %s, %s, %s)`, logFields, resVar, statusCode, dataVar)
+}
+
+func respondText(logFields, resVar, statusCode, dataVar string) string {
+	return fmt.Sprintf(`respondText(%s, %s, %s, %s)`, logFields, resVar, statusCode, dataVar)
+}
+
+func respondEmpty(logFields, resVar, statusCode string) string {
+	return fmt.Sprintf(`respondEmpty(%s, %s, %s)`, logFields, resVar, statusCode)
+}
+
+func generateRespondFunctions(module module.Module) *sources.CodeFile {
+	data := struct {
+		PackageName string
+	}{
+		module.Name,
+	}
+	code := `
+package [[.PackageName]]
+
+import (
+	"encoding/json"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func respondJson(logFields log.Fields, res http.ResponseWriter, statusCode int, data interface{}) {
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(statusCode)
+	json.NewEncoder(res).Encode(data)
+	log.WithFields(logFields).WithField("status", statusCode).Info("Completed request")
+}
+
+func respondText(logFields log.Fields, res http.ResponseWriter, statusCode int, data string) {
+	res.Header().Set("Content-Type", "text/plain")
+	res.WriteHeader(statusCode)
+	res.Write([]byte(data))
+	log.WithFields(logFields).WithField("status", statusCode).Info("Completed request")
+}
+
+func respondEmpty(logFields log.Fields, res http.ResponseWriter, statusCode int) {
+	res.WriteHeader(statusCode)
+	log.WithFields(logFields).WithField("status", statusCode).Info("Completed request")
+}
+`
+
+	code, _ = sources.ExecuteTemplate(code, data)
+	return &sources.CodeFile{module.GetPath("responses.go"), strings.TrimSpace(code)}
+}
