cea86c26f349c3d5285baa4684001246e4b16998
AnastasiyaNislovskaya <nislouskaya.specgen@gmail.com>
2022-07-07 16:26:53 +0300

    client-kotlin-micronaut: Added Micronaut low level client generation

--- gen/kotlin/client/common.go
+++ gen/kotlin/client/common.go
@@ -2,13 +2,10 @@ package client
 
 import (
 	"fmt"
+	"github.com/specgen-io/specgen/v2/generator"
 	"github.com/specgen-io/specgen/v2/spec"
-	"strings"
+	"strconv"
 )
-
-func clientName(api *spec.Api) string {
-	return fmt.Sprintf(`%sClient`, api.Name.PascalCase())
-}
 
 func addBuilderParam(param *spec.NamedParam) string {
 	if param.Type.Definition.IsNullable() {
@@ -17,6 +14,48 @@ func addBuilderParam(param *spec.NamedParam) string {
 	return param.Name.CamelCase()
 }
 
-func joinParams(params []string) string {
-	return strings.Join(params, ", ")
+func generateTryCatch(w *generator.Writer, valName string, exceptionObject string, codeBlock func(w *generator.Writer), exceptionHandler func(w *generator.Writer)) {
+	w.Line(`val %s = try {`, valName)
+	codeBlock(w.Indented())
+	w.Line(`} catch (%s) {`, exceptionObject)
+	exceptionHandler(w.Indented())
+	w.Line(`}`)
+}
+
+func generateClientTryCatch(w *generator.Writer, valName string, statement string, exceptionType, exceptionVar, errorMessage string) {
+	generateTryCatch(w, valName, exceptionVar+`: `+exceptionType,
+		func(w *generator.Writer) {
+			w.Line(statement)
+		},
+		func(w *generator.Writer) {
+			generateThrowClientException(w, errorMessage, exceptionVar)
+		})
+}
+
+func generateThrowClientException(w *generator.Writer, errorMessage string, wrapException string) {
+	w.Line(`val errorMessage = %s`, errorMessage)
+	w.Line(`logger.error(errorMessage)`)
+	params := "errorMessage"
+	if wrapException != "" {
+		params += ", " + wrapException
+	}
+	w.Line(`throw ClientException(%s)`, params)
+}
+
+func isSuccessfulStatusCode(statusCodeStr string) bool {
+	statusCode, _ := strconv.Atoi(statusCodeStr)
+	if statusCode >= 200 && statusCode <= 299 {
+		return true
+	}
+	return false
+}
+
+func responsesNumber(operation *spec.NamedOperation) int {
+	count := 0
+	for _, response := range operation.Responses {
+		if isSuccessfulStatusCode(spec.HttpStatusCode(response.Name)) {
+			count++
+		}
+	}
+	return count
 }
