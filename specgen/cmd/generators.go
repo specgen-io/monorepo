bcc1766d1aff80a66020c69b82def76b6daf2c36
Vladimir Sapronov <vladimir@specgen.io>
2022-08-05 19:41:39 -0400

    spcegen: Created generators package

--- specgen/cmd/generators.go
+++ specgen/cmd/generators.go
@@ -2,36 +2,9 @@ package cmd
 
 import (
 	"github.com/specgen-io/specgen/generator/v2"
-	golang "github.com/specgen-io/specgen/golang/v2/generators"
-	java "github.com/specgen-io/specgen/java/v2/generators"
-	kotlin "github.com/specgen-io/specgen/kotlin/v2/generators"
-	"github.com/specgen-io/specgen/openapi/v2"
-	ruby "github.com/specgen-io/specgen/ruby/v2/generators"
-	scala "github.com/specgen-io/specgen/scala/v2/generators"
-	typescript "github.com/specgen-io/specgen/typescript/v2/generators"
+	"github.com/specgen-io/specgen/v2/generators"
 )
 
 func init() {
-	var generators = []generator.Generator{
-		golang.Models,
-		golang.Client,
-		golang.Service,
-		java.Models,
-		java.Client,
-		java.Service,
-		kotlin.Models,
-		kotlin.Client,
-		kotlin.Service,
-		ruby.Models,
-		ruby.Client,
-		scala.Models,
-		scala.Client,
-		scala.Service,
-		typescript.Models,
-		typescript.Client,
-		typescript.Service,
-		openapi.Openapi,
-	}
-
-	generator.AddCobraCommands(rootCmd, generators)
+	generator.AddCobraCommands(rootCmd, generators.All)
 }
---
+++ specgen/generators/generators.go
@@ -0,0 +1,33 @@
+package generators
+
+import (
+	"github.com/specgen-io/specgen/generator/v2"
+	golang "github.com/specgen-io/specgen/golang/v2/generators"
+	java "github.com/specgen-io/specgen/java/v2/generators"
+	kotlin "github.com/specgen-io/specgen/kotlin/v2/generators"
+	"github.com/specgen-io/specgen/openapi/v2"
+	ruby "github.com/specgen-io/specgen/ruby/v2/generators"
+	scala "github.com/specgen-io/specgen/scala/v2/generators"
+	typescript "github.com/specgen-io/specgen/typescript/v2/generators"
+)
+
+var All = []generator.Generator{
+	golang.Models,
+	golang.Client,
+	golang.Service,
+	java.Models,
+	java.Client,
+	java.Service,
+	kotlin.Models,
+	kotlin.Client,
+	kotlin.Service,
+	ruby.Models,
+	ruby.Client,
+	scala.Models,
+	scala.Client,
+	scala.Service,
+	typescript.Models,
+	typescript.Client,
+	typescript.Service,
+	openapi.Openapi,
+}
