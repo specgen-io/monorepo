a53832c28e68a6c86af192b767b52acab877d816
AnastasiyaNislovskaya <anastasiya@specgen.io>
2022-12-06 19:08:39 +0300

    kotlin-okhttp: Added new tests

--- test-clients/test-service/services/check.go
+++ test-clients/test-service/services/check.go
@@ -3,6 +3,7 @@ package services
 import (
 	"test-service/spec/check"
 	"test-service/spec/empty"
+	"test-service/spec/httperrors/errmodels"
 	"test-service/spec/models"
 )
 
@@ -18,6 +19,14 @@ func (service *CheckService) CheckEmptyResponse(body *models.Message) error {
 
 func (service *CheckService) CheckForbidden() (*check.CheckForbiddenResponse, error) {
 	return &check.CheckForbiddenResponse{Forbidden: &empty.Value}, nil
+}
+
+func (service *CheckService) CheckConflict() (*check.CheckConflictResponse, error) {
+	return &check.CheckConflictResponse{Conflict: &errmodels.ConflictMessage{Message: "Conflict with the current state of the target resource"}}, nil
+}
+
+func (service *CheckService) CheckBadRequest() (*check.CheckBadRequestResponse, error) {
+	return &check.CheckBadRequestResponse{BadRequest: &errmodels.BadRequestError{Location: errmodels.ErrorLocationUnknown, Message: "Failed to execute request", Errors: nil}}, nil
 }
 
 func (service *CheckService) SameOperationName() (*check.SameOperationNameResponse, error) {
