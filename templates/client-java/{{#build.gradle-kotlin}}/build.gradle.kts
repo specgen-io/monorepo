e7749d80b2e11b1f4409f0186060f6c08edd0ec0
AnastasiyaNislovskaya <anastasiya@specgen.io>
2023-02-09 22:38:24 +0300

    micronaut-template: Added Micronaut to client-java template

--- templates/client-java/{{#build.gradle-kotlin}}/build.gradle.kts
+++ templates/client-java/{{#build.gradle-kotlin}}/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    {{#client.micronaut}}
+    id("io.micronaut.application") version "{{versions.micronaut_application.value}}"
+    {{/client.micronaut}}
     id("io.specgen.java.gradle") version "{{versions.specgen.value}}"
 }
 
@@ -11,6 +14,18 @@ repositories {
 }
 
 dependencies {
+{{#client.okhttp}}
+    implementation("com.squareup.okhttp3:okhttp:{{versions.okhttp.value}}")
+    {{/client.okhttp}}
+    {{#client.micronaut}}
+    implementation("io.micronaut:micronaut-http-client")
+    {{#jsonlib.jackson}}
+    implementation("io.micronaut:micronaut-jackson-databind")
+    {{/jsonlib.jackson}}
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut:micronaut-validation")
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    {{/client.micronaut}}
     {{#jsonlib.jackson}}
     implementation("com.fasterxml.jackson.core:jackson-core:{{versions.jackson.value}}")
     implementation("com.fasterxml.jackson.core:jackson-annotations:{{versions.jackson.value}}")
@@ -22,13 +37,13 @@ dependencies {
     implementation("com.squareup.moshi:moshi-adapters:{{versions.moshi.value}}")
     implementation("com.google.code.findbugs:jsr305:{{versions.jsr305.value}}")
     {{/jsonlib.moshi}}
-    implementation("com.squareup.okhttp3:okhttp:{{versions.okhttp.value}}")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:{{versions.log4j.value}}")
 }
 
 specgen {
     clientJava {
         jsonlib.set("{{jsonlib.value}}")
+        client.set("{{client.value}}")
         packageName.set("{{package.value}}")
         specFile.set(file("spec.yaml"))
     }
