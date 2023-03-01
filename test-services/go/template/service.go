38deb5217c6425fc0026fb0a1d663e64c53b9e1a
AnastasiyaNislovskaya <anastasiya@specgen.io>
2023-03-01 20:49:55 +0300

    go-template: Try to fix template

---
+++ test-services/go/template/service.go
@@ -0,0 +1,40 @@
+//go:generate specgen-golang service-go --spec-file ./../spec.yaml --module-name the-service --generate-path ./spec --services-path ./services --swagger-path docs/swagger.yaml
+
+package main
+
+import (
+	"flag"
+	"net/http"
+	"the-service/services"
+	"the-service/services/v2"
+	"the-service/spec"
+
+	"github.com/husobee/vestigo"
+	"github.com/shopspring/decimal"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	port := flag.String("port", "8081", "port number")
+	flag.Parse()
+
+	decimal.MarshalJSONWithoutQuotes = true
+
+	router := vestigo.NewRouter()
+	{{#cors.value}}
+	router.SetGlobalCors(&vestigo.CorsAccessControl{
+		AllowOrigin: []string{"*", "*"},
+	})
+	{{/cors.value}}
+
+	echoServiceV2 := &v2.EchoService{}
+	echoService := &services.EchoService{}
+	checkService := &services.CheckService{}
+
+	spec.AddRoutes(router, echoServiceV2, echoService, checkService)
+
+	router.Get("/docs/*", http.StripPrefix("/docs/", http.FileServer(http.Dir("docs"))).ServeHTTP)
+
+	log.Infof("Starting service on port: %s", *port)
+	log.Fatal(http.ListenAndServe(":"+*port, router))
+}
