02b67a93c25eb660312ff5ced613ed7e1e0cc11e
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2023-05-31 00:48:11 +0100

    Added test-models rendr script

---
+++ test-models/rendr_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash +x
+
+USAGE="usage: ./rendr_tests.sh <GENERATOR> <PARAMS> <SPECGEN_VERSION>"
+
+if [ -n "$1" ]; then
+    GENERATOR=$1
+else
+    echo "Not enough params, ${USAGE}"
+    exit 1
+fi
+
+if [ -n "$2" ]; then
+    PARAMS=$2
+else
+    echo "Not enough params, ${USAGE}"
+    exit 1
+fi
+
+if [ -n "$3" ]; then
+    SPECGEN_VERSION=$3
+else
+    echo "Not enough params, ${USAGE}"
+    exit 1
+fi
+
+OUT_FOLDER=./the-models-${GENERATOR}
+if [ -n "$4" ]; then
+    OUT_FOLDER=$4
+fi
+
+
+rendr file:///../templates/models-${GENERATOR} --root file:///${GENERATOR}/template --noinput --values ${GENERATOR}/${PARAMS}.json --set versions.specgen=${SPECGEN_VERSION} --out ${OUT_FOLDER}
+cp ./spec.yaml ./${OUT_FOLDER}/spec.yaml
