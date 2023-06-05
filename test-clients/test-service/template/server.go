213a8c3e4dea77246667f4ef47102885eccc9e23
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2023-06-05 19:28:36 -0400

    service-go: Fix client test service

--- test-clients/test-service/template/server.go
+++ test-clients/test-service/template/server.go
@@ -6,7 +6,6 @@ import (
 	"flag"
 	"net/http"
 	"the-service/services"
-	"the-service/services/v2"
 	"the-service/spec"
 
 	"github.com/husobee/vestigo"
@@ -28,11 +27,7 @@ func main() {
 		AllowOrigin: []string{"*", "*"},
 	})
 
-	echoServiceV2 := &v2.EchoService{}
-	echoService := &services.EchoService{}
-	checkService := &services.CheckService{}
-
-	spec.AddRoutes(router, echoServiceV2, echoService, checkService)
+	spec.AddRoutes(router, services.Create())
 
 	router.Get("/docs/*", http.StripPrefix("/docs/", http.FileServer(http.Dir("docs"))).ServeHTTP)
 
---
+++ test-clients/test-service/template/services/services.go
@@ -0,0 +1,13 @@
+package services
+
+import (
+	v2 "{{project.value}}/services/v2"
+	"{{project.value}}/spec"
+)
+
+func Create() spec.Services {
+	echoServiceV2 := &v2.EchoService{}
+	echoService := &EchoService{}
+	checkService := &CheckService{}
+	return Services{echoServiceV2, echoService, checkService}
+}
