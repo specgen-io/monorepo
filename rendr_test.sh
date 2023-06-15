6c799fe004ad99665e9cd048c76f15afe49ee17d
Vladimir Sapronov <vladimir@specgen.io>
2023-06-15 15:01:46 -0400

    tests: using latest rendr with verbose mode

--- rendr_test.sh
+++ rendr_test.sh
@@ -31,7 +31,7 @@ TEST_TEMPLATE=./${TESTS_FOLDER}/${LANGUAGE}/template
 PARAMS_FILE=./${TESTS_FOLDER}/${LANGUAGE}/${PARAMS}.yaml
 SPEC_FILE=./${TESTS_FOLDER}/spec.yaml
 
-COMMAND="${RENDR_PATH}rendr file:///${MAIN_TEMPLATE} --root file:///${TEST_TEMPLATE} --noinput --values ${PARAMS_FILE} --set versions.specgen=${SPECGEN_VERSION} --out ${OUT_FOLDER}"
+COMMAND="${RENDR_PATH}rendr file:///${MAIN_TEMPLATE} --root file:///${TEST_TEMPLATE} --noinput --values ${PARAMS_FILE} --set versions.specgen=${SPECGEN_VERSION} --out ${OUT_FOLDER}" --verbose
 echo "Executing: ${COMMAND}"
 $COMMAND
 
