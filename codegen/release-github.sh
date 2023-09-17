72e5c60c6c4d47e646bdb3fad4648e53a3faa950
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2023-09-16 22:44:55 -0400

    github: Fixes for releasing golang tools

--- codegen/release-github.sh
+++ codegen/release-github.sh
@@ -20,12 +20,12 @@ RELEASE_NAME=v$VERSION
 GH_ORG="specgen-io"
 GH_REPO="specgen"
 
-go get github.com/aktau/github-release
+go install github.com/aktau/github-release@v0.10.0
 
 echo "Creating release in Github: $RELEASE_NAME"
 set +e
 GH_PARAMS="--security-token $GH_TOKEN --user $GH_ORG --repo $GH_REPO --tag $RELEASE_NAME"
-$GOPATH/bin/github-release release $GH_PARAMS
+github-release release $GH_PARAMS
 set -e
 
 sleep 10
@@ -33,9 +33,9 @@ sleep 10
 GH_RELEASE_URL="https://github.com/${GH_ORG}/${GH_REPO}/releases/tag/${RELEASE_NAME}"
 echo "Releasing to Github: ${GH_RELEASE_URL}"
 
-$GOPATH/bin/github-release upload --replace $GH_PARAMS --name specgen_darwin_amd64.zip  --file ./specgen_darwin_amd64.zip
-$GOPATH/bin/github-release upload --replace $GH_PARAMS --name specgen_darwin_arm64.zip  --file ./specgen_darwin_arm64.zip
-$GOPATH/bin/github-release upload --replace $GH_PARAMS --name specgen_linux_amd64.zip   --file ./specgen_linux_amd64.zip
-$GOPATH/bin/github-release upload --replace $GH_PARAMS --name specgen_windows_amd64.zip --file ./specgen_windows_amd64.zip
+github-release upload --replace $GH_PARAMS --name specgen_darwin_amd64.zip  --file ./specgen_darwin_amd64.zip
+github-release upload --replace $GH_PARAMS --name specgen_darwin_arm64.zip  --file ./specgen_darwin_arm64.zip
+github-release upload --replace $GH_PARAMS --name specgen_linux_amd64.zip   --file ./specgen_linux_amd64.zip
+github-release upload --replace $GH_PARAMS --name specgen_windows_amd64.zip --file ./specgen_windows_amd64.zip
 
 echo "Done releasing to Github: ${GH_RELEASE_URL}"
