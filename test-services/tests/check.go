c01bca9f8d8dc68fd04b9540c67c74008e3b920a
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2023-09-20 12:30:11 -0400

    tests: service is expected to return parameters instead of query by flag

--- test-services/tests/check.go
+++ test-services/tests/check.go
@@ -1,9 +1,26 @@
 package main
 
-import "os"
+import (
+	"os"
+	"testing"
+)
 
 const ERRORS = "TEST_ERRORS"
+const PARAMETERS_MODE = "TEST_PARAMETERS_MODE"
+const FORM_PARAMS = "TEST_FORM_PARAMS"
 
 func check(name string) bool {
-	return os.Getenv(name) != "false"
+	return os.Getenv(name) == "true"
+}
+
+func skipIf(t *testing.T, name string) {
+	if check(name) {
+		t.SkipNow()
+	}
+}
+
+func skipIfNot(t *testing.T, name string) {
+	if !check(name) {
+		t.SkipNow()
+	}
 }
