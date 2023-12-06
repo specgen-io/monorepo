60ab242734ad952c143cb649b8253dbd8e07aa0f
AnastasiyaNislovskaya <anastasiya@specgen.io>
2023-12-06 19:53:49 +0300

    java-micronaut-form-params: Commented form params in spec

--- test-services/kotlin/template/setup-tests.sh
+++ test-services/kotlin/template/setup-tests.sh
@@ -3,4 +3,7 @@
 {{#server.spring}}
 export TEST_PARAMETERS_MODE=true
 {{/server.spring}}
+{{#server.micronaut}}
+export TEST_COMMA_SEPARATED_FORM_PARAMS_MODE=true
+{{/server.micronaut}}
 export TEST_NO_FORM_DATA=true
